d3d4984a3802a9934ae2e9b129d281651ab902b0
Hongli Lai <hongli@plan99.net>
2008-01-25 19:49:52 +0100

    Add benchmarks

---
+++ benchmark/unix_sockets_vs_pipes.rb
@@ -0,0 +1,79 @@
+#!/usr/bin/env ruby
+require 'benchmark'
+
+def start
+	if ARGV.empty?
+		benchmark(:unix_sockets)
+		benchmark(:pipes)
+	elsif ARGV.size == 1 && ARGV[0] == 'unix_sockets'
+		benchmark(:unix_sockets)
+	elsif ARGV.size == 1 && ARGV[0] == 'pipes'
+		benchmark(:pipes)
+	else
+		puts "Benchmarks Unix socket performance versus pipe performance."
+		puts "Usage: unix_sockets_vs_pipes.rb <unix_sockets|pipes>"
+		exit(1)
+	end
+end
+
+def benchmark(type)
+	if type == :unix_sockets
+		puts "Benchmarking Unix sockets..."
+		pid, reader, writer = setup_unix_sockets
+	else
+		puts "Benchmarking pipes..."
+		pid, reader, writer = setup_pipes
+	end
+	result = Benchmark.measure do
+		150000.times do |i|
+			writer.write("hello world\n")
+			reader.readline
+		end
+		reader.close
+		if reader != writer
+			writer.close
+		end
+		Process.waitpid(pid)
+	end
+	puts "User/system/real time: #{result}"
+end
+
+def setup_unix_sockets
+	require 'socket'
+	a, b = UNIXSocket.pair
+	pid = fork do
+		a.close
+		child_main_loop(b, b)
+	end
+	b.close
+	return [pid, a, a]
+end
+
+def setup_pipes
+	a, b = IO.pipe
+	c, d = IO.pipe
+	pid = fork do
+		b.close
+		c.close
+		reader = a
+		writer = d
+		child_main_loop(reader, writer)
+	end
+	a.close
+	d.close
+	reader = c
+	writer = b
+	return [pid, reader, writer]
+end
+
+def child_main_loop(reader, writer)
+	begin
+		while true
+			x = reader.readline
+			writer.write(x)
+		end
+	rescue EOFError
+	end
+end
+
+start
