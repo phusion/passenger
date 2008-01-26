32c3c25fe7b2f87e17cde9c7eed4c1564735ed8e
Hongli Lai (Phusion) <hongli@phusion.nl>
2008-01-26 20:33:22 +0100

    Almost ready for action

--- ext/apache2/hooks.c
+++ ext/apache2/hooks.c
@@ -21,13 +21,18 @@ inside_base_uri(request_rec *r, RailsConfig *config) {
 }
 
 static INLINE const char *
-determine_rails_root(request_rec *r, RailsConfig *config) {
+determine_rails_dir(request_rec *r, RailsConfig *config) {
 	size_t len = strlen(r->filename) - strlen(r->uri);
 	if (len <= 0) {
 		return NULL;
 	} else {
 		return apr_pstrndup(r->pool, r->filename, len);
 	}
+}
+
+static INLINE bool
+verify_rails_dir(apr_pool_t *pool, const char *dir) {
+	return file_exists(pool, apr_pstrcat(pool, dir, "/../config/environment.rb", NULL));
 }
 
 static int
@@ -42,75 +47,65 @@ mod_rails_handle_request(request_rec *r) {
 	 */
 
 	RailsConfig *config = get_config(r);
-	const char *rails_root;
+	const char *rails_dir;
 	
-	if (!is_well_formed_uri(r->uri) || config->base_uri == NULL) {
-		return DECLINED;
-	} else if (strcmp(r->uri, "mod_rails:htaccess_is_interfering") == 0) {
-		ap_rputs("<b>Error:</b> The \".htaccess\" file in your Ruby on Rails application's "
-			"\"public\" directory is interfering with Apache. Please remove it.", r);
-		return OK;
-	} else if (!inside_base_uri(r, config) || r->filename == NULL || file_exists(r->pool, r->filename)) {
+	if (!is_well_formed_uri(r->uri) || config->base_uri == NULL
+	 || !inside_base_uri(r, config) || r->filename == NULL || file_exists(r->pool, r->filename)) {
 		return DECLINED;
 	}
 	
-	rails_root = determine_rails_root(r, config);
-	if (rails_root) {
-		ap_rputs("", r);
+	rails_dir = determine_rails_dir(r, config);
+	if (rails_dir == NULL) {
+		ap_set_content_type(r, "text/html; charset=UTF-8");
+		ap_rputs("<h1>mod_rails error #1</h1>\n", r);
+		ap_rputs("Cannot determine the location of the Rails application's \"public\" directory.", r);
+		return OK;
+	} else if (!verify_rails_dir(r->pool, rails_dir)) {
+		ap_set_content_type(r, "text/html; charset=UTF-8");
+		ap_rputs("<h1>mod_rails error #2</h1>\n", r);
+		ap_rputs("mod_rails thinks that the Rails application's \"public\" directory is \"", r);
+		ap_rputs(ap_escape_html(r->pool, rails_dir), r);
+		ap_rputs("\", but it doesn't seem to be valid.", r);
 		return OK;
 	}
 	
 	char message[1024];
-	apr_snprintf(message, sizeof(message), "mod_rails: file=%s, uri=%s, root=%s", r->filename, r->uri, determine_rails_root(r, config));
+	apr_snprintf(message, sizeof(message), "mod_rails: file=%s, uri=%s, root=%s", r->filename, r->uri, rails_dir);
 	log_debug(APLOG_MARK, r, message);
 	return DECLINED;
 }
 
 static int
 mod_rails_map_to_storage(request_rec *r) {
-	/* Apache's default map_to_storage process does strange things with the filename.
-	 * Suppose that the DocumentRoot is /website, on server http://test.com/. If we access
-	 * http://test.com/foo/bar, and /website/foo/bar does not exist, then Apache will
-	 * change the filename to /website/foo instead of the expected /website/bar.
-	 * Here, we make sure that doesn't happen.
-	 *
-	 * Incidentally, it also disables mod_rewrite. That is a good thing because the default
-	 * Rails .htaccess file interferes with mod_rails anyway.
-	 */
 	RailsConfig *config = get_config(r);
 	if (!is_well_formed_uri(r->uri) || config->base_uri == NULL || !inside_base_uri(r, config)) {
 		return DECLINED;
 	} else {
-		return OK;
-	}
-}
-
-static int
-mod_rails_check_legacy(request_rec *r) {
-	/* The default Rails .htaccess file interferes with mod_rails because it tries to
-	 * dispatch requests to dispatch.cgi. Here we make sure that mod_rails_handle_request()
-	 * will be able to warn the user if that is the case.
-	 */
-	
-	RailsConfig *config = get_config(r);
-
-	if (!is_well_formed_uri(r->uri) || config->base_uri == NULL || !inside_base_uri(r, config)) {
-		return DECLINED;
-	} else if (r->filename != NULL
-	       && (
-	             strcmp(r->filename, "redirect:/dispatch.cgi") == 0
-	          || strcmp(r->filename, "redirect:/dispatch.fcgi") == 0
-	       )) {
-		r->uri = "mod_rails:htaccess_is_interfering";
-		return OK;
-	} else {
-		return DECLINED;
+		char *html_file = apr_pstrcat(r->pool, r->filename, ".html", NULL);
+		if (file_exists(r->pool, html_file)) {
+			/* If a .html version of the URI exists, serve it directly.
+			 * This is used by page caching.
+			 */
+			r->filename = html_file;
+			r->canonical_filename = html_file;
+			return DECLINED;
+		} else {
+			/* Apache's default map_to_storage process does strange things with the filename.
+			 * Suppose that the DocumentRoot is /website, on server http://test.com/. If we access
+			 * http://test.com/foo/bar, and /website/foo/bar does not exist, then Apache will
+			 * change the filename to /website/foo instead of the expected /website/bar.
+			 * We make sure that doesn't happen.
+			 *
+			 * Incidentally, this also disables mod_rewrite. That is a good thing because
+			 * the default Rails .htaccess file interferes with mod_rails anyway.
+			 */
+			return OK;
+		}
 	}
 }
 
 static void mod_rails_register_hooks(apr_pool_t *p) {
 	ap_hook_post_config(mod_rails_init, NULL, NULL, APR_HOOK_MIDDLE);
-	ap_hook_fixups(mod_rails_check_legacy, NULL, NULL, APR_HOOK_LAST);
 	ap_hook_map_to_storage(mod_rails_map_to_storage, NULL, NULL, APR_HOOK_FIRST);
 	ap_hook_handler(mod_rails_handle_request, NULL, NULL, APR_HOOK_FIRST);
 }
