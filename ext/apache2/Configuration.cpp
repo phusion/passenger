73c6c68f255520eaf4cbd2a4c541ed896eed65fb
Hongli Lai <hongli@plan99.net>
2008-02-07 16:28:38 +0100

    Add support for multiple base URIs. Fix a bug in static asset handling.

--- ext/apache2/Configuration.cpp
+++ ext/apache2/Configuration.cpp
@@ -6,13 +6,13 @@ using namespace std;
 
 static apr_status_t
 destroy_dir_config_struct(void *x) {
-	delete (RailsConfig *) x;
+	delete (RailsDirConfig *) x;
 	return APR_SUCCESS;
 }
 
-static RailsConfig *
+static RailsDirConfig *
 create_dir_config_struct(apr_pool_t *pool) {
-	RailsConfig *config = new RailsConfig();
+	RailsDirConfig *config = new RailsDirConfig();
 	apr_pool_cleanup_register(pool, config, destroy_dir_config_struct, apr_pool_cleanup_null);
 	return config;
 }
@@ -21,21 +21,17 @@ extern "C" {
 
 void *
 passenger_config_create_dir(apr_pool_t *p, char *dirspec) {
-	RailsConfig *config = create_dir_config_struct(p);
-	config->base_uri = NULL;
-	config->base_uri_with_slash = NULL;
+	RailsDirConfig *config = create_dir_config_struct(p);
 	config->env = NULL;
 	return config;
 }
 
 void *
 passenger_config_merge_dir(apr_pool_t *p, void *basev, void *addv) {
-	RailsConfig *config = create_dir_config_struct(p);
-	RailsConfig *base = (RailsConfig *) basev;
-	RailsConfig *add = (RailsConfig *) addv;
+	RailsDirConfig *config = create_dir_config_struct(p);
+	RailsDirConfig *base = (RailsDirConfig *) basev;
+	RailsDirConfig *add = (RailsDirConfig *) addv;
 	
-	config->base_uri = (add->base_uri == NULL) ? base->base_uri : add->base_uri;
-	config->base_uri_with_slash = (add->base_uri_with_slash == NULL) ? base->base_uri_with_slash : add->base_uri_with_slash;
 	config->env = (add->env == NULL) ? base->env : add->env;
 	
 	config->base_uris = base->base_uris;
@@ -48,23 +44,19 @@ passenger_config_merge_dir(apr_pool_t *p, void *basev, void *addv) {
 
 void *
 passenger_config_create_server(apr_pool_t *p, server_rec *s) {
-	return passenger_config_create_dir(p, NULL);
+	//return passenger_config_create_dir(p, NULL);
+	return NULL;
 }
 
 void *
 passenger_config_merge_server(apr_pool_t *p, void *basev, void *overridesv) {
-	return passenger_config_merge_dir(p, basev, overridesv);
+	//return passenger_config_merge_dir(p, basev, overridesv);
+	return NULL;
 }
 
 static const char *
 cmd_rails_base_uri(cmd_parms *cmd, void *pcfg, const char *arg) {
-	RailsConfig *config = (RailsConfig *) pcfg;
-	config->base_uri = arg;
-	if (strcmp(arg, "/") == 0) {
-		config->base_uri_with_slash = "/";
-	} else {
-		config->base_uri_with_slash = apr_pstrcat(cmd->pool, arg, "/", NULL);
-	}
+	RailsDirConfig *config = (RailsDirConfig *) pcfg;
 	config->base_uris.insert(arg);
 	return NULL;
 }
@@ -74,7 +66,7 @@ typedef const char * (*Take1Func)(); // Workaround for some weird C++-specific c
 const command_rec passenger_commands[] = {
 	AP_INIT_TAKE1("RailsBaseURI", (Take1Func) cmd_rails_base_uri, NULL, OR_OPTIONS,
 		"Reserve the given URI to a Rails application."),
-	//AP_INIT_TAKE1("RailsEnv", (Take1Func) ap_set_string_slot, (void *) APR_OFFSETOF(RailsConfig, env), RSRC_CONF,
+	//AP_INIT_TAKE1("RailsEnv", (Take1Func) ap_set_string_slot, (void *) APR_OFFSETOF(RailsDirConfig, env), RSRC_CONF,
 	//	"The environment under which a Rails app must run."),
 	{ NULL }
 };
