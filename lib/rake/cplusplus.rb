7d7ac08a7e685eceafb9466f89cdd5fdc4050ace
Hongli Lai (Phusion) <hongli@phusion.nl>
2008-02-21 15:07:10 +0100

    Attempt to fix compilation on OS X. *Still* not done. :(

--- lib/rake/cplusplus.rb
+++ lib/rake/cplusplus.rb
@@ -1,3 +1,7 @@
+def compile_c(source, flags = CXXFLAGS)
+	sh "#{CXX} #{flags} -c #{source}"
+end
+
 def compile_cxx(source, flags = CXXFLAGS)
 	sh "#{CXX} #{flags} -c #{source}"
 end
@@ -9,4 +13,13 @@ end
 
 def create_executable(target, sources, linkflags = LDFLAGS)
 	sh "#{CXX} #{sources} -o #{target} #{linkflags}"
+end
+
+def create_shared_library(target, sources, flags = LDFLAGS)
+	if RUBY_PLATFORM =~ /darwin/
+		shlib_flag = "-flat_namespace -bundle -undefined dynamic_lookup"
+	else
+		shlib_flag = "-shared"
+	end
+	sh "#{CXX} #{shlib_flag} #{sources} -fPIC -o #{target} #{flags}"
 end
