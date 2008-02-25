0f1f0b1717dbdf1db9e696a0c0e4a7c127a67d5d
Hongli Lai (Phusion) <hongli@phusion.nl>
2008-02-25 13:20:48 +0100

    Improve documentation.

--- lib/mod_rails/cgi_fixed.rb
+++ lib/mod_rails/cgi_fixed.rb
@@ -1,3 +1,29 @@
+# NOTE: This code was copied verbatim from Zed Shaw's scgi_rails
+# package, version 0.4.3. The license of this single file is as follows:
+#
+#  Copyright (c) 2004 Zed A. Shaw
+#  
+#  Permission is hereby granted, free of charge, to any person obtaining
+#  a copy of this software and associated documentation files (the
+#  "Software"), to deal in the Software without restriction, including
+#  without limitation the rights to use, copy, modify, merge, publish,
+#  distribute, sublicense, and/or sell copies of the Software, and to
+#  permit persons to whom the Software is furnished to do so, subject to
+#  the following conditions:
+#  
+#  The above copyright notice and this permission notice shall be
+#  included in all copies or substantial portions of the Software.
+#  
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+#  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+#  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+#  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+#  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+#  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+# We also made some small local modifications.
+
 require 'cgi'
 require 'stringio'
 
@@ -10,8 +36,6 @@ module ModRails # :nodoc:
 #
 # This is partially based on the FastCGI code, but much of the Ruby 1.6 
 # backwards compatibility is removed.
-#
-# DISCLAIMER: This code is copied verbatim from Zed Shaw's scgi_rails package, version 0.4.3.
 class CGIFixed < ::CGI
 	public :env_table
 	
