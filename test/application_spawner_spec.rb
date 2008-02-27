5ec3dc66087734ef6f24c8d77a1eb59c6b3541cb
Hongli Lai (Phusion) <hongli@phusion.nl>
2008-02-28 00:42:25 +0100

    Refactor tests for ApplicationSpawner: split most stuff to external specifications, for later reuse.

--- test/application_spawner_spec.rb
+++ test/application_spawner_spec.rb
@@ -1,6 +1,7 @@
 require 'support/config'
 require 'mod_rails/application_spawner'
-require 'abstract_server_spec'
+require 'minimal_spawner_spec'
+require 'spawn_server_spec'
 require 'spawner_privilege_lowering_spec'
 include ModRails
 
@@ -24,45 +25,11 @@ describe ApplicationSpawner do
 		@spawner.stop
 	end
 	
-	it_should_behave_like "AbstractServer"
+	it_should_behave_like "a minimal spawner"
+	it_should_behave_like "a spawn server"
 	
-	it "should be able to spawn our stub application" do
-		app = @spawner.spawn_application
-		app.pid.should_not == 0
-		app.app_root.should_not be_nil
-		app.close
-	end
-	
-	it "should be able to spawn an arbitary number of applications" do
-		last_pid = 0
-		4.times do
-			app = @spawner.spawn_application
-			app.pid.should_not == last_pid
-			app.app_root.should_not be_nil
-			last_pid = app.pid
-			app.close
-		end
-	end
-	
-	it "should raise a SpawnError if something went wrong" do
-		pid = @spawner.instance_eval { @pid }
-		Process.kill('SIGABRT', pid)
-		spawning = lambda { @spawner.spawn_application }
-		spawning.should raise_error(ApplicationSpawner::SpawnError)
-	end
-	
-	it "should work correctly after a restart, if something went wrong" do
-		pid = @spawner.instance_eval { @pid }
-		Process.kill('SIGABRT', pid)
-		spawning = lambda { @spawner.spawn_application }
-		spawning.should raise_error(ApplicationSpawner::SpawnError)
-		
-		@spawner.stop
-		@spawner.start
-		app = @spawner.spawn_application
-		app.pid.should_not == 0
-		app.app_root.should_not be_nil
-		app.close
+	def spawn_application
+		@spawner.spawn_application
 	end
 end
 
---
+++ test/spawn_server_spec.rb
@@ -0,0 +1,25 @@
+require 'support/config'
+require 'abstract_server_spec'
+
+shared_examples_for "a spawn server" do
+	it_should_behave_like "AbstractServer"
+	
+	it "should raise a SpawnError if something went wrong" do
+		Process.kill('SIGABRT', @spawner.server_pid)
+		spawning = lambda { spawn_application }
+		spawning.should raise_error(ApplicationSpawner::SpawnError)
+	end
+	
+	it "should work correctly after a restart, if something went wrong" do
+		Process.kill('SIGABRT', @spawner.server_pid)
+		spawning = lambda { spawn_application }
+		spawning.should raise_error(ApplicationSpawner::SpawnError)
+		
+		@spawner.stop
+		@spawner.start
+		app = spawn_application
+		app.pid.should_not == 0
+		app.app_root.should_not be_nil
+		app.close
+	end
+end
