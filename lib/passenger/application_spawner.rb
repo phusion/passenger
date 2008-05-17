f4e4e229fc0266b57e13b2116cf89e1a1ca4ee6b
Hongli Lai (Phusion) <hongli@phusion.nl>
2008-05-17 18:10:48 +0200

    Ignore errors in initgroups. Some systems are broken.

--- lib/passenger/application_spawner.rb
+++ lib/passenger/application_spawner.rb
@@ -226,7 +226,10 @@ private
 		if uid == ROOT_UID
 			return false
 		else
-			Process.groups = Process.initgroups(username, gid)
+			# Some systems are broken. initgroups can fail because of
+			# all kinds of stupid reasons. So we ignore any errors
+			# raised by initgroups.
+			Process.groups = Process.initgroups(username, gid) rescue nil
 			Process::Sys.setgid(gid)
 			Process::Sys.setuid(uid)
 			return true
