549775e31dd7c5b6f14f3cfa6412f01d009937b3
Hongli Lai (Phusion) <hongli@phusion.nl>
2008-05-17 22:11:03 +0200

    Fix mistake.

--- lib/passenger/utils.rb
+++ lib/passenger/utils.rb
@@ -237,7 +237,10 @@ protected
 			# Some systems are broken. initgroups can fail because of
 			# all kinds of stupid reasons. So we ignore any errors
 			# raised by initgroups.
-			Process.groups = Process.initgroups(username, gid) rescue nil
+			begin
+				Process.groups = Process.initgroups(username, gid)
+			rescue
+			end
 			Process::Sys.setgid(gid)
 			Process::Sys.setuid(uid)
 			ENV['HOME'] = pw.dir
