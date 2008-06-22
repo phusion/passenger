9346eebb2e6d2fd2cb09ebf26b74ea88bfabd2e8
Hongli Lai (Phusion <hongli@phusion.nl>
2008-06-22 16:03:55 +0200

    Fix more compilation problems on OpenBSD. Still doesn't work though: the unit tests crash on startup.

--- ext/oxt/spin_lock.hpp
+++ ext/oxt/spin_lock.hpp
@@ -25,9 +25,16 @@
 #ifndef _OXT_SPIN_LOCK_HPP_
 #define _OXT_SPIN_LOCK_HPP_
 
-#if (defined(__GNUC__) && defined(__i386__)) || defined(IN_DOXYGEN)
+// At the time of writing (June 22, 2008), these operating systems don't
+// support pthread spin locks:
+// - OpenBSD 4.3
+#if defined(__OpenBSD__)
+	#define OXT_NO_PTHREAD_SPINLOCKS
+#endif
+
+#if ((__GNUC__ >= 4) && defined(__i386__)) || defined(IN_DOXYGEN)
 	#include "detail/spin_lock_gcc_x86.hpp"
-#elif !defined(WIN32)
+#elif !defined(WIN32) && !defined(OXT_NO_PTHREAD_SPINLOCKS)
 	#include "detail/spin_lock_pthreads.hpp"
 #else
 	#include "detail/spin_lock_portable.hpp"
