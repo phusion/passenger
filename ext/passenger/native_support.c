57860807dcb8027e1a9fde89bea4ff89fabe7250
Hongli Lai (Phusion) <hongli@phusion.nl>
2008-12-05 01:59:45 +0100

    Fix source compatibility with Ruby 1.8.7.

--- ext/passenger/native_support.c
+++ ext/passenger/native_support.c
@@ -242,7 +242,7 @@ close_all_file_descriptors(VALUE self, VALUE exceptions) {
 	
 	for (i = sysconf(_SC_OPEN_MAX) - 1; i >= 0; i--) {
 		int is_exception = 0;
-		for (j = 0; j < RARRAY(exceptions)->len && !is_exception; j++) {
+		for (j = 0; j < RARRAY_LEN(exceptions) && !is_exception; j++) {
 			long fd = NUM2INT(rb_ary_entry(exceptions, j));
 			is_exception = i == fd;
 		}
