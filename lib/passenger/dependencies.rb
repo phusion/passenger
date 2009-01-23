f7f042caac683bcfc411d93c299e96ee6636fb42
Hongli Lai (Phusion) <hongli@phusion.nl>
2009-01-23 23:14:01 +0100

    Correctly report APR/APU as not found if they're not found, instead of wrongly reporting that they're found. Fix typo.

--- lib/passenger/dependencies.rb
+++ lib/passenger/dependencies.rb
@@ -257,7 +257,11 @@ module Dependencies # :nodoc: all
 	APR_DevHeaders = Dependency.new do |dep|
 		dep.name = "Apache Portable Runtime (APR) development headers"
 		dep.define_checker do |result|
-			result.found(APR_CONFIG)
+			if APR_CONFIG
+				result.found(APR_CONFIG)
+			else
+				result.not_found
+			end
 		end
 		if RUBY_PLATFORM =~ /linux/
 			case LINUX_DISTRO
@@ -278,9 +282,13 @@ module Dependencies # :nodoc: all
 	end
 
 	APU_DevHeaders = Dependency.new do |dep|
-		dep.name = "Apache Portable Runtime Utility (APR) development headers"
+		dep.name = "Apache Portable Runtime Utility (APU) development headers"
 		dep.define_checker do |result|
-			result.found(APU_CONFIG)
+			if APU_CONFIG
+				result.found(APU_CONFIG)
+			else
+				result.not_found
+			end
 		end
 		dep.website = "http://httpd.apache.org/"
 		dep.website_comments = "APR Utility is an integrated part of Apache."
