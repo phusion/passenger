8c75e95902d30fa27a67c538c0ffc4c9c714d6a8
Hongli Lai (Phusion) <hongli@phusion.nl>
2009-03-31 22:59:44 +0200

    Update the vendored Rack library to commit 4cc1d6e773f64c20849b1d8088717a084b06830a. Fixes issue #220.

--- lib/phusion_passenger/rack/application_spawner.rb
+++ lib/phusion_passenger/rack/application_spawner.rb
@@ -14,7 +14,7 @@
 #  with this program; if not, write to the Free Software Foundation, Inc.,
 #  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-$LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__) + "/../../../vendor/rack-0.9.1/lib"))
+$LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__) + "/../../../vendor/rack-0.9.1-git/lib"))
 require 'rack'
 
 require 'socket'
