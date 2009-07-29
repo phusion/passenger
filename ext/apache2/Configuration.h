a682aa5604daaaed546ce32092925a593f6a753c
Andre Ferraz <ferraz@ahfalo.fabrica.locaweb.com.br>
2009-07-29 17:59:36 -0300

    fixing Typo bugs

--- ext/apache2/Configuration.h
+++ ext/apache2/Configuration.h
@@ -135,7 +135,7 @@
 			bool memoryLimitSpecified;
 			
             /** Flag created to resolve issue of symlinks on the DocumentRoot configuration */
-            bool docRootresolveSymlink;
+            bool docRootResolveSymlink;
 
 			Threeway highPerformance;
 			
@@ -173,7 +173,7 @@
 			
 			string getAppRoot(const char *documentRoot) const {
 				if (appRoot == NULL) {
-                    if (docRootresolveSymlink == DirConfig::ENABLED) {
+                    if (docRootResolveSymlink == DirConfig::ENABLED) {
                         return extractDirName(resolveSymlink(documentRoot));
                     } else {
     					return extractDirName(documentRoot);
@@ -185,7 +185,7 @@
 			
 			string getAppRoot(const string &documentRoot) const {
 				if (appRoot == NULL) {
-                    if (docRootresolveSymlink == DirConfig::ENABLED) {
+                    if (docRootResolveSymlink == DirConfig::ENABLED) {
                         return extractDirName(resolveSymlink(documentRoot));
                     } else {
     					return extractDirName(documentRoot);
