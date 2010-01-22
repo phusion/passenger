ab5acdf6d0f0f1962beb699106b53ec906a39e18
Hongli Lai (Phusion) <hongli@phusion.nl>
2010-01-22 14:49:16 +0100

    Fix a compilation warning (must check return value on fwrite).

--- ext/nginx/ngx_http_passenger_module.c
+++ ext/nginx/ngx_http_passenger_module.c
@@ -178,26 +178,31 @@ starting_helper_server_after_fork(void *arg) {
 
 static ngx_int_t
 create_file(ngx_cycle_t *cycle, const u_char *filename, const u_char *contents, size_t len) {
-	FILE *f;
-	int   ret;
-	
-	f = fopen((const char *) filename, "w");
-	if (f != NULL) {
-		/* We must do something with these return values because
-		 * otherwise on some platforms it will cause a compiler
-		 * warning.
-		 */
-		do {
-			ret = fchmod(fileno(f), S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-		} while (ret == -1 && errno == EINTR);
-		fwrite(contents, 1, len, f);
-		fclose(f);
-		return NGX_OK;
-	} else {
-		ngx_log_error(NGX_LOG_ALERT, cycle->log, ngx_errno,
-			"could not create %s", filename);
-		return NGX_ERROR;
-	}
+    FILE  *f;
+    int    ret;
+    size_t total_written = 0, written;
+
+    f = fopen((const char *) filename, "w");
+    if (f != NULL) {
+        /* We must do something with these return values because
+         * otherwise on some platforms it will cause a compiler
+         * warning.
+         */
+        do {
+            ret = fchmod(fileno(f), S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+        } while (ret == -1 && errno == EINTR);
+        do {
+            written = fwrite(contents + total_written, 1,
+                len - total_written, f);
+            total_written += written;
+        } while (total_written < len);
+        fclose(f);
+        return NGX_OK;
+    } else {
+        ngx_log_error(NGX_LOG_ALERT, cycle->log, ngx_errno,
+            "could not create %s", filename);
+            return NGX_ERROR;
+    }
 }
 
 /**
