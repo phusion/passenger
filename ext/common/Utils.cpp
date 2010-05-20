phusion/passenger

commit 775e220f08d302ae633ce3d65063528cb86efd54

Fix Nginx "passenger_user_switching off" permission problems: issue #458.

ext/common/Utils.cpp
@@ -359,7 +359,14 @@ createNonWritableFifo(const string &filename) {
 	do {
 		ret = mkfifo(filename.c_str(), 0);
 	} while (ret == -1 && errno == EINTR);
-	if (ret == -1 && errno != EEXIST) {
+	if (ret == -1) {
+		if (errno == EEXIST && geteuid() != 0) {
+			// Don't try to change the permissions on the FIFO file;
+			// it was likely created by root, but after lowering privilege
+			// createPassengerTempDir() is called again, and this time
+			// we won't be able to set permissions.
+			return;
+		}
 		e = errno;
 		throw FileSystemException("Cannot create FIFO file " + filename, e, filename);
 	}

commit eb512a535a04023c6d8daaf8f68f95fcec99c95b

Commit 775e220 introduced a regression: EEXIST isn't supposed to cause an exception to be thrown. Fix this.

ext/common/Utils.cpp
@@ -355,26 +355,30 @@ setPassengerTempDir(const string &dir) {
 static void
 createNonWritableFifo(const string &filename) {
 	int ret, e;
+	bool ignoreChmodErrors = false;
 	
 	do {
 		ret = mkfifo(filename.c_str(), 0);
 	} while (ret == -1 && errno == EINTR);
 	if (ret == -1) {
-		if (errno == EEXIST && geteuid() != 0) {
-			// Don't try to change the permissions on the FIFO file;
-			// it was likely created by root, but after lowering privilege
-			// createPassengerTempDir() is called again, and this time
-			// we won't be able to set permissions.
-			return;
+		if (errno == EEXIST) {
+			/* The FIFO file was likely created by root, but after lowering
+			 * privilege createPassengerTempDir() is called again, and this
+			 * time we won't be able to set permissions. So in this case
+			 * we'll want to ignore any chmod errors.
+			 */
+			ignoreChmodErrors = geteuid() != 0;
+		} else {
+			e = errno;
+			throw FileSystemException("Cannot create FIFO file " + filename,
+				e, filename);
 		}
-		e = errno;
-		throw FileSystemException("Cannot create FIFO file " + filename, e, filename);
 	}
 	
 	do {
 		ret = chmod(filename.c_str(), 0);
 	} while (ret == -1 && errno == EINTR);
-	if (ret == -1) {
+	if (ret == -1 && !ignoreChmodErrors) {
 		e = errno;
 		throw FileSystemException("Cannot set permissions on file " + filename, e, filename);
 	}