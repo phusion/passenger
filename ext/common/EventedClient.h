fedcc237a00a3ac4f5c18defdd834cd3c00fa004
Hongli Lai (Phusion) <hongli@phusion.nl>
2010-08-04 16:49:29 +0200

    Make sure that EventedClient unregisters watchers before closing the file descriptor.

--- ext/common/EventedClient.h
+++ ext/common/EventedClient.h
@@ -366,7 +366,12 @@ public:
 		writeWatcher.set(fd, ev::WRITE);
 	}
 	
-	virtual ~EventedClient() { }
+	virtual ~EventedClient() {
+		// Unregister file descriptor from the event loop poller before
+		// closing the file descriptor.
+		watchReadEvents(false);
+		watchWriteEvents(false);
+	}
 	
 	/**
 	 * Increase reference count.
