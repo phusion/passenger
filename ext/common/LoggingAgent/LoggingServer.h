ebad4fe6ac9e442ee0c9bdc5a89984b3b51f0473
Hongli Lai (Phusion) <hongli@phusion.nl>
2010-08-26 11:14:19 +0200

    Flush log file sinks every 15 seconds instead of every 5 seconds, remote sinks every 60 seconds instead of every 30 seconds.

--- ext/common/LoggingAgent/LoggingServer.h
+++ ext/common/LoggingAgent/LoggingServer.h
@@ -827,10 +827,10 @@ private:
 		for (it = logSinkCache.begin(); it != end; it++) {
 			LogSink *sink = it->second.get();
 			
-			// Flush log file sinks every 5 seconds,
-			// remote sinks every 30 seconds.
+			// Flush log file sinks every 15 seconds,
+			// remote sinks every 60 seconds.
 			if (sink->isRemote()) {
-				if (now - sink->lastFlushed >= 30) {
+				if (now - sink->lastFlushed >= 60) {
 					sink->flush();
 				}
 			} else {
@@ -1223,7 +1223,7 @@ public:
 		garbageCollectionTimer.set<LoggingServer, &LoggingServer::garbageCollect>(this);
 		garbageCollectionTimer.start(GARBAGE_COLLECTION_TIMEOUT, GARBAGE_COLLECTION_TIMEOUT);
 		sinkFlushingTimer.set<LoggingServer, &LoggingServer::sinkFlushTimeout>(this);
-		sinkFlushingTimer.start(5, 5);
+		sinkFlushingTimer.start(15, 15);
 		exitTimer.set<LoggingServer, &LoggingServer::exitTimerTimeout>(this);
 		exitTimer.set(0.05, 0.05);
 		refuseNewConnections = false;
