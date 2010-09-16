fbd0af073de13b1319a306e1b42d6ba8bcd0a06b
Hongli Lai (Phusion) <hongli@phusion.nl>
2010-09-16 10:55:59 +0200

    Fix RVM gemset detection in latest RVM releases. Thanks Wayne!

--- lib/phusion_passenger/platform_info/ruby.rb
+++ lib/phusion_passenger/platform_info/ruby.rb
@@ -27,6 +27,15 @@ require 'phusion_passenger/platform_info'
 module PhusionPassenger
 
 module PlatformInfo
+	# Store original $GEM_HOME value so that even if the app customizes
+	# $GEM_HOME we can still work with the original value.
+	gem_home = ENV['GEM_HOME']
+	if gem_home
+		gem_home = gem_home.strip.freeze
+		gem_home = nil if gem_home.empty?
+	end
+	GEM_HOME = gem_home
+	
 	if defined?(::RUBY_ENGINE)
 		RUBY_ENGINE = ::RUBY_ENGINE
 	else
@@ -37,6 +46,25 @@ module PlatformInfo
 	# In case of RVM this function will return the path to the RVM wrapper script
 	# that executes the current Ruby interpreter in the currently active gem set.
 	def self.ruby_command
+		if in_rvm?
+			name = rvm_ruby_string
+			dir = rvm_path
+			if name && dir
+				return "#{dir}/wrappers/#{name}/ruby"
+			else
+				# Something's wrong with the user's RVM installation.
+				# Raise an error so that the user knows this instead of
+				# having things fail randomly later on.
+				# 'name' is guaranteed to be non-nil because rvm_ruby_string
+				# already raises an exception on error.
+				raise "Your RVM installation appears to be broken: the RVM " +
+					"path cannot be found. Please fix your RVM installation " +
+					"or contact the RVM developers for support."
+			end
+		else
+			return ruby_executable
+		end
+		
 		@@ruby_command ||= begin
 			filename = ruby_executable
 			if filename =~ %r{(.*)/.rvm/rubies/(.+?)/bin/(.+)}
@@ -54,6 +82,7 @@ module PlatformInfo
 			filename
 		end
 	end
+	memoize :ruby_command
 	
 	# Returns the full path to the current Ruby interpreter's executable file.
 	# This might not be the actual correct command to use for invoking the Ruby
@@ -121,6 +150,68 @@ module PlatformInfo
 	def self.in_rvm?
 		return Config::CONFIG['bindir'].include?('/.rvm/')
 	end
+	
+	# If the current Ruby interpreter is managed by RVM, returns the
+	# directory in which RVM places its working files. Otherwise returns
+	# nil.
+	def self.rvm_path
+		if in_rvm?
+			[ENV['rvm_path'], "~/.rvm", "/usr/local/rvm"].each do |path|
+				next if path.nil?
+				path = File.expand_path(path)
+				return path if File.directory?(path)
+			end
+		end
+		return nil
+	end
+	memoize :rvm_path
+	
+	# If the current Ruby interpreter is managed by RVM, returns the
+	# RVM name which identifies the current Ruby interpreter plus the
+	# currently active gemset, e.g. something like this:
+	# "ruby-1.9.2-p0@mygemset"
+	#
+	# Returns nil otherwise.
+	def self.rvm_ruby_string
+		if in_rvm?
+			# RVM used to export the necessary information through
+			# environment variables, but doesn't always do that anymore
+			# in the latest versions in order to fight env var pollution.
+			# Scanning $LOAD_PATH seems to be the only way to obtain
+			# the information.
+			
+			# Getting the RVM name of the Ruby interpreter ("ruby-1.9.2")
+			# isn't so hard, we can extract it from the #ruby_executable
+			# string. Getting the gemset name is a bit harder, so let's
+			# try various strategies...
+			
+			# $GEM_HOME usually contains the gem set name.
+			if GEM_HOME && GEM_HOME.include?("rvm/gems/")
+				return File.basename(GEM_HOME)
+			end
+			
+			# User somehow managed to nuke $GEM_HOME. Extract info
+			# from $LOAD_PATH.
+			matching_path = $LOAD_PATH.find_all do |item|
+				item.include?("rvm/gems/")
+			end
+			if matching_path
+				subpath = matching_path.to_s.gsub(/^.*rvm\/gems\//, '')
+				result = subpath.split('/').first
+				return result if result
+			end
+			
+			# On Ruby 1.9, $LOAD_PATH does not contain any gem paths until
+			# at least one gem has been required so the above can fail.
+			# We're out of options now, we can't detect the gem set.
+			# Raise an exception so that the user knows what's going on
+			# instead of having things fail in obscure ways later.
+			raise "Unable to autodetect the currently active RVM gem " +
+				"set name. Please contact this program's author for support."
+		end
+		return nil
+	end
+	memoize :rvm_ruby_string
 	
 	# Returns either 'sudo' or 'rvmsudo' depending on whether the current
 	# Ruby interpreter is managed by RVM.
