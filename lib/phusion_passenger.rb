fb6c421ad0c70e2088e76f6a1d879be4c064a152
Hongli Lai (Phusion) <hongli@phusion.nl>
2010-09-20 17:56:43 +0200

    Bump version to 3.0.0.pre3

--- lib/phusion_passenger.rb
+++ lib/phusion_passenger.rb
@@ -25,7 +25,7 @@ module PhusionPassenger
 	###### Version numbers ######
 	
 	# Phusion Passenger version number. Don't forget to edit ext/common/Constants.h too.
-	VERSION_STRING = '3.0.0.pre2'
+	VERSION_STRING = '3.0.0.pre3'
 	
 	PREFERRED_NGINX_VERSION = '0.7.67'
 	PREFERRED_PCRE_VERSION  = '8.02'
