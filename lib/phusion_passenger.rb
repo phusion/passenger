b8aedafbbcf606d595100e9518c0d030cd97e233
Hongli Lai (Phusion) <hongli@phusion.nl>
2011-11-26 09:51:44 +0100

    Bump version number to 3.0.10.

--- lib/phusion_passenger.rb
+++ lib/phusion_passenger.rb
@@ -1,5 +1,5 @@
 #  Phusion Passenger - http://www.modrails.com/
-#  Copyright (c) 2010 Phusion
+#  Copyright (c) 2010, 2011 Phusion
 #
 #  "Phusion Passenger" is a trademark of Hongli Lai & Ninh Bui.
 #
@@ -25,7 +25,7 @@ module PhusionPassenger
 	###### Version numbers ######
 	
 	# Phusion Passenger version number. Don't forget to edit ext/common/Constants.h too.
-	VERSION_STRING = '3.0.9'
+	VERSION_STRING = '3.0.10'
 	
 	PREFERRED_NGINX_VERSION = '1.0.10'
 	PREFERRED_PCRE_VERSION  = '8.12'
