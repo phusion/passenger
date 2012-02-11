4117ed828954ee2402b3d5bcce659e66124aa251
Hongli Lai (Phusion) <hongli@phusion.nl>
2012-02-11 13:59:48 +0100

    Use StdioGuard in ProcessMetricsCollector.

--- ext/common/Utils/ProcessMetricsCollector.h
+++ ext/common/Utils/ProcessMetricsCollector.h
@@ -1,6 +1,6 @@
 /*
  *  Phusion Passenger - http://www.modrails.com/
- *  Copyright (c) 2010 Phusion
+ *  Copyright (c) 2010, 2011, 2012 Phusion
  *
  *  "Phusion Passenger" is a trademark of Hongli Lai & Ninh Bui.
  *
@@ -518,7 +518,7 @@ public:
 				return;
 			}
 			
-			ScopeGuard fileGuard(boost::bind(syscalls::fclose, f));
+			StdioGuard guard(f);
 			bool hasPss = false;
 			bool hasPrivateDirty = false;
 			bool hasSwap = false;
