96bdfe37dc2fdc4c1e7e943aba64b290c186a8dd
Hongli Lai (Phusion) <hongli@phusion.nl>
2012-02-13 09:56:29 +0100

    Add copyright notices.

--- ext/common/EventedBufferedInput.h
+++ ext/common/EventedBufferedInput.h
@@ -1,3 +1,27 @@
+/*
+ *  Phusion Passenger - http://www.modrails.com/
+ *  Copyright (c) 2011, 2012 Phusion
+ *
+ *  "Phusion Passenger" is a trademark of Hongli Lai & Ninh Bui.
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
 #ifndef _EVENTED_BUFFERED_INPUT_H_
 #define _EVENTED_BUFFERED_INPUT_H_
 
@@ -20,6 +44,18 @@ namespace Passenger {
 using namespace boost;
 using namespace oxt;
 
+/**
+ * Provides input buffering services for non-blocking sockets in evented I/O systems.
+ *
+ * Wrap an EventedBufferedInput around a socket and provide a data handler callback.
+ * The handler is called every time there is incoming socket data. The handler must return
+ * the number of bytes that it has actually consumed. If not everything has been
+ * consumed, then the handler will be called with the remaining data in the next
+ * tick.
+ *
+ * TODO: this code is directly ported from Zangetsu's socket_input_wrapper.js. We
+ * should port over the unit tests too.
+ */
 template<size_t bufferSize = 1024 * 8>
 class EventedBufferedInput: public enable_shared_from_this< EventedBufferedInput<bufferSize> > {
 private:
