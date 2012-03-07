66114e5bf916e8a8ccee5ef97d57fb28f4a8813d
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2012-03-07 13:42:51 +0100

    add extconf for building the native gem

---
+++ ext/apache2/extconf.rb
@@ -0,0 +1,22 @@
+m = open("Makefile", "wb")
+PASSENGER_ROOT = File.expand_path(File.dirname(__FILE__) + "/../..")
+data =<<EOF
+SHELL=/bin/sh
+
+PASSENGER_DIR=#{PASSENGER_ROOT}
+
+all install static install-so install-rb:
+\t[ -d $(PASSENGER_DIR) ] || (echo "ERROR: Passenger dir $(PASSENGER_DIR) not found" && exit)
+\techo "Installing passenger"
+\tUSE_VENDORED_LIBEV=no $(PASSENGER_DIR)/bin/passenger-install-apache2-module --auto
+\tcd $(PASSENGER_DIR) && find ext/apache2 -name \*.a -print -delete
+\tcd $(PASSENGER_DIR) && rm -v test/stub/wsgi/passenger_wsgi.pyc  2> /dev/null || echo "passenger_wsgi.pyc already removed"
+\tcd $(PASSENGER_DIR) && mv -v ext/ruby/*/passenger_native_support.so lib/ 2>/dev/null || [ -f lib/passenger_native_support.so ] ||echo "ERROR: lib/passenger_native_support.so not found."
+\tcd $(PASSENGER_DIR) && rm -rv ext/ruby/ruby-*-linux || echo "ruby-*-linux already removed"
+\tsed -i -e "s,@PassengerRoot@,$(PASSENGER_DIR)," config/mod_passenger_root.include
+
+
+EOF
+m.puts data
+m.close
+
