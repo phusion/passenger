36ea58a3ac7eeac7234217f129d6e8105da4dd9c
Hongli Lai (Phusion) <hongli@phusion.nl>
2012-03-19 15:44:47 +0100

    Update NEWS and show at least 'Phusion Passenger' in the Nginx Server header if passenger_show_version_in_header is false.

--- ext/nginx/ContentHandler.c
+++ ext/nginx/ContentHandler.c
@@ -1281,7 +1281,7 @@ process_header(ngx_http_request_t *r)
                 h->key.len = sizeof("Server") - 1;
                 h->key.data = (u_char *) "Server";
                 if( slcf->show_version_in_header == 0 ) {
-                    h->value.data = (u_char *) (NGINX_VER);
+                    h->value.data = (u_char *) (NGINX_VER " + Phusion Passenger (mod_rails/mod_rack)");
                 } else {
                     h->value.data = (u_char *) (NGINX_VER " + Phusion Passenger " PASSENGER_VERSION " (mod_rails/mod_rack)");
                 }
