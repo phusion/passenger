8cc43c13beab1712ddae55c53a92ba19e0e1fe42
Hongli Lai (Phusion) <hongli@phusion.nl>
2012-05-27 20:45:01 +0200

    Fix some bugs in the classic Rails loader's error reporting code.

--- helper-scripts/classic-rails-preloader.rb
+++ helper-scripts/classic-rails-preloader.rb
@@ -111,20 +111,29 @@ module App
 		puts "!> I have control 1.0"
 		abort "Invalid initialization header" if STDIN.readline != "You have control 1.0\n"
 		
-		while (line = STDIN.readline) != "\n"
-			name, value = line.strip.split(/: */, 2)
-			options[name] = value
+		begin
+			while (line = STDIN.readline) != "\n"
+				name, value = line.strip.split(/: */, 2)
+				options[name] = value
+			end
+			@@options = LoaderSharedHelpers.sanitize_spawn_options(@@options)
+			
+			if Rails::VERSION::STRING >= '2.3.0'
+				require 'phusion_passenger/rack/request_handler'
+				handler = Rack::RequestHandler.new(STDIN, ActionController::Dispatcher.new, options)
+			else
+				require 'phusion_passenger/classic_rails/request_handler'
+				handler = ClassicRails::RequestHandler.new(STDIN, options)
+			end
+			LoaderSharedHelpers.before_handling_requests(true, options)
+		rescue Exception => e
+			LoaderSharedHelpers.about_to_abort(e)
+			puts "!> Error"
+			puts "!> "
+			puts format_exception(e)
+			exit exit_code_for_exception(e)
 		end
-		@@options = LoaderSharedHelpers.sanitize_spawn_options(@@options)
-		
-		if Rails::VERSION::STRING >= '2.3.0'
-			require 'phusion_passenger/rack/request_handler'
-			handler = Rack::RequestHandler.new(STDIN, ActionController::Dispatcher.new, options)
-		else
-			require 'phusion_passenger/classic_rails/request_handler'
-			handler = ClassicRails::RequestHandler.new(STDIN, options)
-		end
-		LoaderSharedHelpers.before_handling_requests(true, options)
+
 		puts "!> Ready"
 		LoaderSharedHelpers.advertise_sockets(STDOUT, handler)
 		puts "!> "
