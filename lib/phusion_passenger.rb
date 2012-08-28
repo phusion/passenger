184f47e4c0a5fd2699228733b916630048173688
Hongli Lai (Phusion) <hongli@phusion.nl>
2012-08-28 19:03:41 +0200

    Fix conflict

--- lib/phusion_passenger.rb
+++ lib/phusion_passenger.rb
@@ -29,11 +29,7 @@ module PhusionPassenger
 	###### Version numbers ######
 	
 	# Phusion Passenger version number. Don't forget to edit ext/common/Constants.h too.
-<<<<<<< HEAD
 	VERSION_STRING = '3.1.0'
-=======
-	VERSION_STRING = '3.0.17'
->>>>>>> master
 	
 	PREFERRED_NGINX_VERSION = '1.2.3'
 	PREFERRED_PCRE_VERSION  = '8.31'
