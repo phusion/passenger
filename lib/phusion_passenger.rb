2993f58cd608a69f3c4396c1e79c0592737babea
Hongli Lai (Phusion) <hongli@phusion.nl>
2013-04-02 17:03:34 +0200

    Update NEWS, bump version to 4.0.0 RC 6.

--- lib/phusion_passenger.rb
+++ lib/phusion_passenger.rb
@@ -31,7 +31,7 @@ module PhusionPassenger
 	PACKAGE_NAME = 'passenger'
 	
 	# Phusion Passenger version number. Don't forget to edit ext/common/Constants.h too.
-	VERSION_STRING = '4.0.0.rc5'
+	VERSION_STRING = '4.0.0.rc6'
 	
 	PREFERRED_NGINX_VERSION = '1.2.7'
 	PREFERRED_PCRE_VERSION  = '8.32'
