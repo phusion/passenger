29118a78bf273471d91c88aa88d70bc969c1da4b
Hongli Lai (Phusion) <hongli@phusion.nl>
2013-05-21 20:56:10 +0200

    Bump version 4.0.3.

--- lib/phusion_passenger.rb
+++ lib/phusion_passenger.rb
@@ -31,7 +31,7 @@ module PhusionPassenger
 	PACKAGE_NAME = 'passenger'
 	
 	# Phusion Passenger version number. Don't forget to edit ext/common/Constants.h too.
-	VERSION_STRING = '4.0.2'
+	VERSION_STRING = '4.0.3'
 	
 	PREFERRED_NGINX_VERSION = '1.4.1'
 	NGINX_SHA256_CHECKSUM = 'bca5d1e89751ba29406185e1736c390412603a7e6b604f5b4575281f6565d119'
