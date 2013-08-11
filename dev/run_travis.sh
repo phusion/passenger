f371fa67196a3acf85e129d8f0c7c59a2876a598
Hongli Lai (Phusion) <hongli@phusion.nl>
2013-08-11 17:26:51 +0200

    The Debian packaging Travis tests should install rubygems so that it works on Apachai Hopachai

--- dev/run_travis.sh
+++ dev/run_travis.sh
@@ -75,7 +75,7 @@ if [[ "$TEST_DEBIAN_PACKAGING" = 1 ]]; then
 	run sudo apt-get update
 	run sudo apt-get install -y --no-install-recommends \
 		devscripts debhelper rake apache2-mpm-worker apache2-threaded-dev \
-		ruby1.8 ruby1.8-dev ruby1.9.1 ruby1.9.1-dev libev-dev gdebi-core \
+		ruby1.8 ruby1.8-dev ruby1.9.1 ruby1.9.1-dev rubygems libev-dev gdebi-core \
 		source-highlight
 	run rake test:install_deps RAILS_BUNDLES=no
 	run rake debian:dev
