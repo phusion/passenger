e93eb5f665ef29ac66d70637d979d2f38c90eaff
Hongli Lai (Phusion) <hongli@phusion.nl>
2013-08-24 13:59:59 +0200

    Explicitly set permissions on the server instance directory with chmod, so that permissions are correct on systems with a non-default umask. Fixes issue #928.

--- ext/common/ServerInstanceDir.h
+++ ext/common/ServerInstanceDir.h
@@ -251,6 +251,12 @@ private:
 			throw FileSystemException("Cannot create server instance directory '" +
 				path + "'", e, path);
 		}
+		// Explicitly chmod the directory in case the umask is interfering.
+		if (chmod(path.c_str(), parseModeString("u=rwx,g=rx,o=rx")) == -1) {
+			int e = errno;
+			throw FileSystemException("Cannot set permissions on server instance directory '" +
+				path + "'", e, path);
+		}
 		// verifyDirectoryPermissions() checks for the owner/group so we must make
 		// sure the server instance directory has that owner/group, even when the
 		// parent directory has setgid on.
