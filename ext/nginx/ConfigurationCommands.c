phusion/passenger

commit da74ba2d0df2340b6b7069a75e6f8263dd5882f7

Split Nginx configuration definitions into a definition file and auto-generate boilerplate code from that.

ext/nginx/ConfigurationCommands.c
@@ -1,0 +1,441 @@
+/*
+ *  Phusion Passenger - https://www.phusionpassenger.com/
+ *  Copyright (c) 2010-2013 Phusion
+ *
+ *  "Phusion Passenger" is a trademark of Hongli Lai & Ninh Bui.
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+/*
+ * ConfigurationCommands.c is automatically generated from ConfigurationCommands.c.erb,
+ * using definitions from lib/phusion_passenger/nginx/config_options.rb.
+ * Edits to ConfigurationCommands.c will be lost.
+ *
+ * To update ConfigurationCommands.c:
+ *   rake nginx
+ *
+ * To force regeneration of ConfigurationCommands.c:
+ *   rm -f ext/nginx/ConfigurationCommands.c
+ *   rake ext/nginx/ConfigurationCommands.c
+ */
+
+
+
+
+{
+	
+	ngx_string("passenger_enabled"),
+	NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_FLAG,
+	passenger_enabled,
+	NGX_HTTP_LOC_CONF_OFFSET,
+	offsetof(passenger_loc_conf_t, enabled),
+	NULL
+},
+
+{
+	
+	ngx_string("passenger_ruby"),
+	NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_TAKE1,
+	ngx_conf_set_str_slot,
+	NGX_HTTP_LOC_CONF_OFFSET,
+	offsetof(passenger_loc_conf_t, ruby),
+	NULL
+},
+
+{
+	
+	ngx_string("passenger_python"),
+	NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_TAKE1,
+	ngx_conf_set_str_slot,
+	NGX_HTTP_LOC_CONF_OFFSET,
+	offsetof(passenger_loc_conf_t, python),
+	NULL
+},
+
+{
+	
+	ngx_string("passenger_app_env"),
+	NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_TAKE1,
+	ngx_conf_set_str_slot,
+	NGX_HTTP_LOC_CONF_OFFSET,
+	offsetof(passenger_loc_conf_t, environment),
+	NULL
+},
+
+{
+	
+	ngx_string("passenger_friendly_error_pages"),
+	NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_FLAG,
+	ngx_conf_set_flag_slot,
+	NGX_HTTP_LOC_CONF_OFFSET,
+	offsetof(passenger_loc_conf_t, friendly_error_pages),
+	NULL
+},
+
+{
+	
+	ngx_string("passenger_min_instances"),
+	NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_TAKE1,
+	ngx_conf_set_num_slot,
+	NGX_HTTP_LOC_CONF_OFFSET,
+	offsetof(passenger_loc_conf_t, min_instances),
+	NULL
+},
+
+{
+	
+	ngx_string("passenger_max_requests"),
+	NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_TAKE1,
+	ngx_conf_set_num_slot,
+	NGX_HTTP_LOC_CONF_OFFSET,
+	offsetof(passenger_loc_conf_t, max_requests),
+	NULL
+},
+
+{
+	
+	ngx_string("passenger_base_uri"),
+	NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_TAKE1,
+	ngx_conf_set_str_array_slot,
+	NGX_HTTP_LOC_CONF_OFFSET,
+	offsetof(passenger_loc_conf_t, base_uris),
+	NULL
+},
+
+{
+	
+	ngx_string("passenger_user"),
+	NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_TAKE1,
+	ngx_conf_set_str_slot,
+	NGX_HTTP_LOC_CONF_OFFSET,
+	offsetof(passenger_loc_conf_t, user),
+	NULL
+},
+
+{
+	
+	ngx_string("passenger_group"),
+	NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_TAKE1,
+	ngx_conf_set_str_slot,
+	NGX_HTTP_LOC_CONF_OFFSET,
+	offsetof(passenger_loc_conf_t, group),
+	NULL
+},
+
+{
+	
+	ngx_string("passenger_app_group_name"),
+	NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_TAKE1,
+	ngx_conf_set_str_slot,
+	NGX_HTTP_LOC_CONF_OFFSET,
+	offsetof(passenger_loc_conf_t, app_group_name),
+	NULL
+},
+
+{
+	
+	ngx_string("passenger_app_root"),
+	NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_TAKE1,
+	ngx_conf_set_str_slot,
+	NGX_HTTP_LOC_CONF_OFFSET,
+	offsetof(passenger_loc_conf_t, app_root),
+	NULL
+},
+
+{
+	
+	ngx_string("passenger_app_rights"),
+	NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_TAKE1,
+	ngx_conf_set_str_slot,
+	NGX_HTTP_LOC_CONF_OFFSET,
+	offsetof(passenger_loc_conf_t, app_rights),
+	NULL
+},
+
+{
+	
+	ngx_string("union_station_support"),
+	NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_FLAG,
+	ngx_conf_set_flag_slot,
+	NGX_HTTP_LOC_CONF_OFFSET,
+	offsetof(passenger_loc_conf_t, union_station_support),
+	NULL
+},
+
+{
+	
+	ngx_string("union_station_filter"),
+	NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_TAKE1,
+	union_station_filter,
+	NGX_HTTP_LOC_CONF_OFFSET,
+	offsetof(passenger_loc_conf_t, union_station_filters),
+	NULL
+},
+
+{
+	
+	ngx_string("passenger_debugger"),
+	NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_FLAG,
+	ngx_conf_set_flag_slot,
+	NGX_HTTP_LOC_CONF_OFFSET,
+	offsetof(passenger_loc_conf_t, debugger),
+	NULL
+},
+
+{
+	
+	ngx_string("passenger_show_version_in_header"),
+	NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_FLAG,
+	ngx_conf_set_flag_slot,
+	NGX_HTTP_LOC_CONF_OFFSET,
+	offsetof(passenger_loc_conf_t, show_version_in_header),
+	NULL
+},
+
+{
+	
+	ngx_string("passenger_max_preloader_idle_time"),
+	NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_TAKE1,
+	ngx_conf_set_num_slot,
+	NGX_HTTP_LOC_CONF_OFFSET,
+	offsetof(passenger_loc_conf_t, max_preloader_idle_time),
+	NULL
+},
+
+{
+	
+	ngx_string("passenger_ignore_headers"),
+	NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_1MORE,
+	ngx_conf_set_bitmask_slot,
+	NGX_HTTP_LOC_CONF_OFFSET,
+	offsetof(passenger_loc_conf_t, upstream_config.ignore_headers),
+	NULL
+},
+
+{
+	
+	ngx_string("passenger_set_cgi_param"),
+	NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_TAKE2,
+	set_null_terminated_keyval_slot,
+	NGX_HTTP_LOC_CONF_OFFSET,
+	offsetof(passenger_loc_conf_t, vars_source),
+	NULL
+},
+
+{
+	
+	ngx_string("passenger_pass_header"),
+	NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_TAKE1,
+	ngx_conf_set_str_array_slot,
+	NGX_HTTP_LOC_CONF_OFFSET,
+	offsetof(passenger_loc_conf_t, upstream_config.pass_headers),
+	NULL
+},
+
+{
+	
+	ngx_string("passenger_ignore_client_abort"),
+	NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_FLAG,
+	ngx_conf_set_flag_slot,
+	NGX_HTTP_LOC_CONF_OFFSET,
+	offsetof(passenger_loc_conf_t, upstream_config.ignore_client_abort),
+	NULL
+},
+
+{
+	
+	ngx_string("passenger_buffer_response"),
+	NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_FLAG,
+	ngx_conf_set_flag_slot,
+	NGX_HTTP_LOC_CONF_OFFSET,
+	offsetof(passenger_loc_conf_t, upstream_config.buffering),
+	NULL
+},
+
+{
+	
+	ngx_string("passenger_buffer_size"),
+	NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_TAKE1,
+	ngx_conf_set_size_slot,
+	NGX_HTTP_LOC_CONF_OFFSET,
+	offsetof(passenger_loc_conf_t, upstream_config.buffer_size),
+	NULL
+},
+
+{
+	
+	ngx_string("passenger_buffers"),
+	NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_TAKE2,
+	ngx_conf_set_bufs_slot,
+	NGX_HTTP_LOC_CONF_OFFSET,
+	offsetof(passenger_loc_conf_t, upstream_config.bufs),
+	NULL
+},
+
+{
+	
+	ngx_string("passenger_busy_buffers_size"),
+	NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_TAKE1,
+	ngx_conf_set_size_slot,
+	NGX_HTTP_LOC_CONF_OFFSET,
+	offsetof(passenger_loc_conf_t, upstream_config.busy_buffers_size_conf),
+	NULL
+},
+
+{
+	
+	ngx_string("passenger_spawn_method"),
+	NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_TAKE1,
+	ngx_conf_set_str_slot,
+	NGX_HTTP_LOC_CONF_OFFSET,
+	offsetof(passenger_loc_conf_t, spawn_method),
+	NULL
+},
+
+{
+	
+	ngx_string("union_station_key"),
+	NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_TAKE1,
+	ngx_conf_set_str_slot,
+	NGX_HTTP_LOC_CONF_OFFSET,
+	offsetof(passenger_loc_conf_t, union_station_key),
+	NULL
+},
+
+{
+	
+	ngx_string("passenger_max_instances"),
+	NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_TAKE1,
+	passenger_enterprise_only,
+	NGX_HTTP_LOC_CONF_OFFSET,
+	0,
+	NULL
+},
+
+{
+	
+	ngx_string("passenger_max_request_time"),
+	NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_TAKE1,
+	passenger_enterprise_only,
+	NGX_HTTP_LOC_CONF_OFFSET,
+	0,
+	NULL
+},
+
+{
+	
+	ngx_string("passenger_memory_limit"),
+	NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_TAKE1,
+	passenger_enterprise_only,
+	NGX_HTTP_LOC_CONF_OFFSET,
+	0,
+	NULL
+},
+
+{
+	
+	ngx_string("passenger_concurrency_model"),
+	NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_TAKE1,
+	passenger_enterprise_only,
+	NGX_HTTP_LOC_CONF_OFFSET,
+	0,
+	NULL
+},
+
+{
+	
+	ngx_string("passenger_thread_count"),
+	NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_TAKE1,
+	passenger_enterprise_only,
+	NGX_HTTP_LOC_CONF_OFFSET,
+	0,
+	NULL
+},
+
+{
+	
+	ngx_string("passenger_rolling_restarts"),
+	NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_FLAG,
+	passenger_enterprise_only,
+	NGX_HTTP_LOC_CONF_OFFSET,
+	0,
+	NULL
+},
+
+{
+	
+	ngx_string("passenger_resist_deployment_errors"),
+	NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_FLAG,
+	passenger_enterprise_only,
+	NGX_HTTP_LOC_CONF_OFFSET,
+	0,
+	NULL
+},
+
+{
+	
+	ngx_string("rails_spawn_method"),
+	NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_TAKE1,
+	ngx_conf_set_str_slot,
+	NGX_HTTP_LOC_CONF_OFFSET,
+	offsetof(passenger_loc_conf_t, spawn_method),
+	NULL
+},
+
+{
+	
+	ngx_string("rails_env"),
+	NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_TAKE1,
+	ngx_conf_set_str_slot,
+	NGX_HTTP_LOC_CONF_OFFSET,
+	offsetof(passenger_loc_conf_t, environment),
+	NULL
+},
+
+{
+	
+	ngx_string("rack_env"),
+	NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_TAKE1,
+	ngx_conf_set_str_slot,
+	NGX_HTTP_LOC_CONF_OFFSET,
+	offsetof(passenger_loc_conf_t, environment),
+	NULL
+},
+
+{
+	
+	ngx_string("rails_framework_spawner_idle_time"),
+	NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_TAKE1,
+	rails_framework_spawner_idle_time,
+	NGX_HTTP_LOC_CONF_OFFSET,
+	0,
+	NULL
+},
+
+{
+	
+	ngx_string("passenger_use_global_queue"),
+	NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_FLAG,
+	passenger_use_global_queue,
+	NGX_HTTP_LOC_CONF_OFFSET,
+	0,
+	NULL
+},
+

lib/phusion_passenger/nginx/config_options.rb
@@ -1,0 +1,297 @@
+#  Phusion Passenger - https://www.phusionpassenger.com/
+#  Copyright (c) 2013 Phusion
+#
+#  "Phusion Passenger" is a trademark of Hongli Lai & Ninh Bui.
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#  THE SOFTWARE.
+
+# This file defines all supported Nginx per-location configuration options. The
+# build system automatically generates the corresponding Nginx module boilerplate
+# code from the definitions in this file.
+# 
+# Main configuration options are not defined in this file, but are defined in
+# ext/nginx/Configuraion.c instead.
+# 
+# The following boilerplate code is generated:
+# 
+#  * ngx_command_t array members (ConfigurationCommands.c.erb)
+#  * Location configuration structure definition (ConfigurationFields.h.erb)
+#  * Location configuration structure initialization (CreateLocationConfig.c.erb)
+#  * Location configuration merging (MergeLocationConfig.c.erb)
+#  * Conversion of configuration options to CGI headers (CacheLocationConfig.c.erb)
+# 
+# Options:
+# 
+#  * name - The configuration option name. Required.
+#  * context - The context in which this configuration option is valid.
+#              Defaults to [:main, :srv, :loc, :lif]
+#  * type - This configuration option's value type. Allowed types:
+#           :string, :integer, :flag, :string_array, :string_keyval
+#  * take - Tells Nginx how many parameters and what kind of parameter
+#           this configuration option takes. It should be set to a string
+#           such as "NGX_CONF_FLAG".
+#           By default this is automatically inferred from `type`: for
+#           example if `type` is :string then ConfigurationCommands.c.erb
+#           will infer that `NGX_CONF_TAKE1` should be used.
+#  * function - The name of the function that should be used to store the
+#               configuration value into the corresponding structure. This function
+#               is not auto-generated, so it must be the name of an existing
+#               function. By default, the function name is automatically inferred
+#               from `type`. For example if `type` is string then `function` is
+#               inferred to be `ngx_conf_set_str_slot`.
+#               If you set this to a string then you are responsible for defining
+#               said function in Configuration.c.
+#  * field - The name that should be used for the auto-generated field in
+#            the location configuration structure. Defaults to the configuration
+#            name without the 'passenger_' prefix. Set this to nil if you do not
+#            want a structure field to be auto-generated. If the field name contains
+#            a dot (.e.g `upstream_config.pass_headers`) then the structure field will
+#            also not be auto-generated, because it is assumed to belong to an existing
+#            structure field.
+#  * header - The name of the corresponding CGI header. By default CGI header
+#             generation code is automatically generated, using the configuration
+#             option's name in uppercase as the CGI header name.
+#             Setting this to nil, or setting `field` to a value containing a dot,
+#             will disable auto-generation of CGI header generation code. You are
+#             then responsible for writing CGI header passing code yourself in
+#             ContentHandler.c.
+#  * auto_generate_nginx_merge_code - Whether location configuration merging
+#            code should be automatically generated. Defaults to true. If you set
+#            this to false then you are responsible for writing merging code
+#            yourself in Configuration.c.
+#  * alias_for - Set this if this configuration option is an alias for another
+#                option. Alias definitions must only have the `name` and `alias_for`
+#                fields, nothing else.
+
+
+LOCATION_CONFIGURATION_OPTIONS = [
+	{
+		:name     => 'passenger_enabled',
+		:context  => [:srv, :loc, :lif],
+		:type     => :flag,
+		:function => 'passenger_enabled',
+		:field    => 'enabled',
+		:header   => nil
+	},
+	{
+		:name    => 'passenger_ruby',
+		:context => [:srv, :loc, :lif],
+		:type    => :string
+	},
+	{
+		:name  => 'passenger_python',
+		:type  => :string
+	},
+	{
+		:name  => 'passenger_app_env',
+		:type  => :string,
+		:field => 'environment',
+		:header => 'PASSENGER_ENV'
+	},
+	{
+		:name  => 'passenger_friendly_error_pages',
+		:type  => :flag
+	},
+	{
+		:name  => 'passenger_min_instances',
+		:type  => :integer
+	},
+	{
+		:name  => 'passenger_max_requests',
+		:type  => :integer
+	},
+	{
+		:name   => 'passenger_base_uri',
+		:type   => :string_array,
+		:field  => 'base_uris',
+		:header => nil,
+		:auto_generate_nginx_merge_code => false
+	},
+	{
+		:name  => 'passenger_user',
+		:type  => :string
+	},
+	{
+		:name  => 'passenger_group',
+		:type  => :string
+	},
+	{
+		:name  => 'passenger_app_group_name',
+		:type  => :string
+	},
+	{
+		:name  => 'passenger_app_root',
+		:type  => :string
+	},
+	{
+		:name => 'passenger_app_rights',
+		:type => :string
+	},
+	{
+		:name  => 'union_station_support',
+		:type  => :flag
+	},
+	{
+		:name     => 'union_station_filter',
+		:take     => 'NGX_CONF_TAKE1',
+		:type     => :string_array,
+		:function => 'union_station_filter',
+		:field    => 'union_station_filters',
+		:header   => nil,
+		:auto_generate_nginx_merge_code => false
+	},
+	{
+		:name  => 'passenger_debugger',
+		:type  => :flag
+	},
+	{
+		:name  => 'passenger_show_version_in_header',
+		:type  => :flag
+	},
+	{
+		:name  => 'passenger_max_preloader_idle_time',
+		:type  => :integer
+	},
+	{
+		:name     => 'passenger_ignore_headers',
+		:take     => 'NGX_CONF_1MORE',
+		:function => 'ngx_conf_set_bitmask_slot',
+		:field    => 'upstream_config.ignore_headers'
+	},
+	{
+		:name   => 'passenger_set_cgi_param',
+		:type   => :string_keyval,
+		:field  => 'vars_source',
+		:header => nil,
+		:auto_generate_nginx_merge_code => false
+	},
+	{
+		:name  => 'passenger_pass_header',
+		:type  => :string_array,
+		:field => 'upstream_config.pass_headers'
+	},
+	{
+		:name  => 'passenger_ignore_client_abort',
+		:type  => :flag,
+		:field => 'upstream_config.ignore_client_abort'
+	},
+	{
+		:name  => 'passenger_buffer_response',
+		:type  => :flag,
+		:field => 'upstream_config.buffering'
+	},
+	{
+		:name     => 'passenger_buffer_size',
+		:take     => 'NGX_CONF_TAKE1',
+		:function => 'ngx_conf_set_size_slot',
+		:field    => 'upstream_config.buffer_size'
+	},
+	{
+		:name     => 'passenger_buffers',
+		:take     => 'NGX_CONF_TAKE2',
+		:function => 'ngx_conf_set_bufs_slot',
+		:field    => 'upstream_config.bufs'
+	},
+	{
+		:name     => 'passenger_busy_buffers_size',
+		:take     => 'NGX_CONF_TAKE1',
+		:function => 'ngx_conf_set_size_slot',
+		:field    => 'upstream_config.busy_buffers_size_conf'
+	},
+	{
+		:name  => 'passenger_spawn_method',
+		:type  => :string
+	},
+	{
+		:name  => 'union_station_key',
+		:type  => :string
+	},
+
+	###### Enterprise features ######
+	{
+		:name     => 'passenger_max_instances',
+		:type     => :integer,
+		:function => 'passenger_enterprise_only',
+		:field    => nil
+	},
+	{
+		:name     => 'passenger_max_request_time',
+		:type     => :integer,
+		:function => 'passenger_enterprise_only',
+		:field    => nil
+	},
+	{
+		:name     => 'passenger_memory_limit',
+		:type     => :integer,
+		:function => 'passenger_enterprise_only',
+		:field    => nil
+	},
+	{
+		:name     => 'passenger_concurrency_model',
+		:type     => :string,
+		:function => 'passenger_enterprise_only',
+		:field    => nil
+	},
+	{
+		:name     => 'passenger_thread_count',
+		:type     => :integer,
+		:function => 'passenger_enterprise_only',
+		:field    => nil
+	},
+	{
+		:name     => 'passenger_rolling_restarts',
+		:type     => :flag,
+		:function => 'passenger_enterprise_only',
+		:field    => nil
+	},
+	{
+		:name     => 'passenger_resist_deployment_errors',
+		:type     => :flag,
+		:function => 'passenger_enterprise_only',
+		:field    => nil
+	},
+
+	###### Aliases for backwards compatibility ######
+	{
+		:name      => 'rails_spawn_method',
+		:alias_for => 'passenger_spawn_method'
+	},
+	{
+		:name      => 'rails_env',
+		:alias_for => 'passenger_app_env'
+	},
+	{
+		:name      => 'rack_env',
+		:alias_for => 'passenger_app_env'
+	},
+
+	###### Obsolete options ######
+	{
+		:name     => 'rails_framework_spawner_idle_time',
+		:take     => 'NGX_CONF_TAKE1',
+		:function => 'rails_framework_spawner_idle_time',
+		:field    => nil
+	},
+	{
+		:name     => 'passenger_use_global_queue',
+		:take     => 'NGX_CONF_FLAG',
+		:function => 'passenger_use_global_queue',
+		:field    => nil
+	}
+]

build/nginx.rb
@@ -21,6 +21,14 @@
 #  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 #  THE SOFTWARE.
 
+auto_generated_sources = [
+	'ext/nginx/ConfigurationCommands.c',
+	'ext/nginx/CreateLocationConfig.c',
+	'ext/nginx/MergeLocationConfig.c',
+	'ext/nginx/CacheLocationConfig.c',
+	'ext/nginx/ConfigurationFields.h'
+]
+
 desc "Build Nginx support files"
 task :nginx => [
 	:nginx_without_native_support,
@@ -28,6 +36,7 @@ task :nginx => [
 ].compact
 
 task :nginx_without_native_support => [
+	auto_generated_sources,
 	AGENT_OUTPUT_DIR + 'PassengerHelperAgent',
 	AGENT_OUTPUT_DIR + 'PassengerWatchdog',
 	AGENT_OUTPUT_DIR + 'PassengerLoggingAgent',
@@ -39,4 +48,19 @@ task :clean => 'nginx:clean'
 desc "Clean all compiled Nginx files"
 task 'nginx:clean' => 'common:clean' do
 	# Nothing to clean at this time.
+end
+
+def create_nginx_auto_generated_source_task(source)
+	dependencies = [
+		"#{source}.erb",
+		'lib/phusion_passenger/nginx/config_options.rb'
+	]
+	file(source => dependencies) do
+		template = TemplateRenderer.new("#{source}.erb")
+		template.render_to(source)
+	end
+end
+
+auto_generated_sources.each do |source|
+	create_nginx_auto_generated_source_task(source)
 end

ext/common/ApplicationPool2/Options.h
@@ -370,8 +370,8 @@ public:
 		baseURI                 = "/";
 		spawnMethod             = "smart";
 		defaultUser             = "nobody";
-		ruby                    = "ruby";
-		python                  = "python";
+		ruby                    = DEFAULT_RUBY;
+		python                  = DEFAULT_PYTHON;
 		rights                  = DEFAULT_BACKEND_ACCOUNT_RIGHTS;
 		debugger                = false;
 		loadShellEnvvars        = true;

commit 48e88a211ac1c46640b7586cc8fe4d6e4bab9a5c

Fix rails_app_spawner_idle_time, which commit da74ba2d broke

ext/nginx/ConfigurationCommands.c
@@ -441,6 +441,16 @@
 
 {
 	
+	ngx_string("rails_app_spawner_idle_time"),
+	NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_TAKE1,
+	ngx_conf_set_num_slot,
+	NGX_HTTP_LOC_CONF_OFFSET,
+	offsetof(passenger_loc_conf_t, max_preloader_idle_time),
+	NULL
+},
+
+{
+	
 	ngx_string("rails_framework_spawner_idle_time"),
 	NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LIF_CONF | NGX_CONF_TAKE1,
 	rails_framework_spawner_idle_time,

lib/phusion_passenger/nginx/config_options.rb
@@ -294,6 +294,10 @@ LOCATION_CONFIGURATION_OPTIONS = [
 		:name      => 'rack_env',
 		:alias_for => 'passenger_app_env'
 	},
+	{
+		:name      => 'rails_app_spawner_idle_time',
+		:alias_for => 'passenger_max_preloader_idle_time'
+	},
 
 	###### Obsolete options ######
 	{