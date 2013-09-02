91c154d041904624abe60214765f0fa6a21b6a7f
Hongli Lai (Phusion) <hongli@phusion.nl>
2013-09-02 11:31:28 -0300

    Fix typos

--- ext/nginx/ConfigurationFields.h
+++ ext/nginx/ConfigurationFields.h
@@ -26,7 +26,7 @@
 /*
  * ConfigurationFields.h is automatically generated from ConfigurationFields.h.erb,
  * using definitions from lib/phusion_passenger/nginx/config_options.rb.
- * Edits to MergeLocationConfig.c will be lost.
+ * Edits to ConfigurationFields.h will be lost.
  *
  * To update ConfigurationFields.h:
  *   rake nginx
