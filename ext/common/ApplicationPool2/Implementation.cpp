ea11d28c0f22a54f599809e54b9dc5ec55de47f0
Hongli Lai (Phusion) <hongli@phusion.nl>
2013-10-12 13:30:39 +0200

    always_restart.txt should be in tmp

--- ext/common/ApplicationPool2/Implementation.cpp
+++ ext/common/ApplicationPool2/Implementation.cpp
@@ -330,7 +330,7 @@ Group::Group(const SuperGroupPtr &_superGroup, const Options &options, const Com
 	lifeStatus     = ALIVE;
 	if (options.restartDir.empty()) {
 		restartFile = options.appRoot + "/tmp/restart.txt";
-		alwaysRestartFile = options.appRoot + "/always_restart.txt";
+		alwaysRestartFile = options.appRoot + "/tmp/always_restart.txt";
 	} else if (options.restartDir[0] == '/') {
 		restartFile = options.restartDir + "/restart.txt";
 		alwaysRestartFile = options.restartDir + "/always_restart.txt";
