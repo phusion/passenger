73076a42a65eced5e656e6205d3e2fa749cd7236
Hongli Lai (Phusion) <hongli@phusion.nl>
2013-12-25 11:40:28 +0100

    Split the passenger-config command into multiple files with a dispatcher.

---
+++ lib/phusion_passenger/config.rb
@@ -0,0 +1,117 @@
+#  Phusion Passenger - https://www.phusionpassenger.com/
+#  Copyright (c) 2013 Phusion
+#
+#  "Phusion Passenger" is a trademark of Hongli Lai & Ninh Bui.
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#  THE SOFTWARE.
+
+PhusionPassenger.require_passenger_lib 'constants'
+
+module PhusionPassenger
+
+# Core of the `passenger-config` command. Dispatches a subcommand to a specific class.
+module Config
+	KNOWN_COMMANDS = [
+		["info", "InfoCommand"]
+	]
+	
+	INFO_OPTIONS = [
+		"root",
+		"includedir",
+		"nginx-addon-dir",
+		"nginx-libs",
+		"compiled",
+		"natively-packaged",
+		"installed-from-release-package",
+		"make-locations-ini",
+		"detect-apache2",
+		"ruby-command",
+		"ruby-libdir",
+		"rubyext-compat-id",
+		"cxx-compat-id",
+		"version"
+	]
+
+	def self.run!(argv)
+		command_class, new_argv = lookup_command_class_by_argv(argv)
+		if help_requested?(argv)
+			help
+		elsif command_class
+			command = command_class.new(new_argv)
+			command.run
+		else
+			help
+			abort
+		end
+	end
+
+	def self.help
+		puts "Usage: passenger-config <COMMAND> [options]"
+		puts "Tool for controlling or configurating a #{PROGRAM_NAME} instance or installation."
+		puts
+		puts "Available commands:"
+		KNOWN_COMMANDS.each do |props|
+			command_class = lookup_command_class_by_class_name(props[1])
+			printf "  %-15s %s\n", props[0], command_class.description
+		end
+	end
+
+private
+	def self.help_requested?(argv)
+		return argv[0] == "--help" || argv[0] == "-h" || argv[0] == "help"
+	end
+
+	def self.lookup_command_class_by_argv(argv)
+		return nil if argv.empty?
+
+		# Compatibility with version <= 4.0.29: try to pass all
+		# --switch invocations to InfoCommand.
+		if argv[0] =~ /^--/
+			name = argv[0].sub(/^--/, '')
+			if INFO_OPTIONS.include?(name)
+				command_class = lookup_command_class_by_class_name("InfoCommand")
+				return [command_class, argv]
+			else
+				return nil
+			end
+		end
+
+		KNOWN_COMMANDS.each do |props|
+			if argv[0] == props[0]
+				command_class = lookup_command_class_by_class_name(props[1])
+				new_argv = argv[1 .. -1]
+				return [command_class, new_argv]
+			end
+		end
+
+		return nil
+	end
+
+	def self.lookup_command_class_by_class_name(class_name)
+		base_name = class_name.gsub(/[A-Z]/) do |match|
+			"_" + match[0].downcase
+		end
+		base_name.sub!(/^_/, '')
+		base_name << ".rb"
+		PhusionPassenger.require_passenger_lib("config/#{base_name}")
+		return PhusionPassenger::Config.const_get(class_name)
+	end
+end
+
+end # module PhusionPassenger
---
+++ lib/phusion_passenger/config/info_command.rb
@@ -0,0 +1,181 @@
+#  Phusion Passenger - https://www.phusionpassenger.com/
+#  Copyright (c) 2010-2013 Phusion
+#
+#  "Phusion Passenger" is a trademark of Hongli Lai & Ninh Bui.
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#  THE SOFTWARE.
+
+PhusionPassenger.require_passenger_lib 'constants'
+PhusionPassenger.require_passenger_lib 'config/command'
+
+module PhusionPassenger
+module Config
+
+class InfoCommand < Command
+	def self.description
+		return "Show information about #{PROGRAM_NAME}"
+	end
+
+	def self.help
+		puts "Usage: passenger-config info <SUBCOMMAND>"
+		puts "Show information about #{PROGRAM_NAME}."
+		puts
+		puts "Available subcommands:"
+		puts "  root                     Show Phusion Passenger's root directory."
+		puts "  includedir               Show the Nginx runtime library headers directory."
+		puts "  nginx-addon-dir          Show the Phusion Passenger Nginx addon directory."
+		puts "  nginx-libs               Show Nginx runtime library flags."
+		puts "  compiled                 Check whether runtime libraries are compiled."
+		puts "  natively-packaged        Check whether Phusion Passenger is natively"
+		puts "                           packaged."
+		puts "  installed-from-release-package  Check whether this installation came from"
+		puts "                                  an official release package."
+		puts "  make-locations-ini       Generate a locations.ini based on the current"
+		puts "                           install paths."
+		puts "  detect-apache2           Autodetect Apache installations."
+		puts "  ruby-command             Print the correct command for invoking the Ruby"
+		puts "                           interpreter."
+		puts "  ruby-libdir              Show Phusion Passenger's Ruby library directory."
+		puts "  rubyext-compat-id        Print the Ruby extension binary compatibility ID."
+		puts "  cxx-compat-id            Print the C++ binary compatibility ID."
+		puts "  version                  Show version number."
+	end
+
+	def run
+		PhusionPassenger.require_passenger_lib 'platform_info'
+		PhusionPassenger.require_passenger_lib 'platform_info/compiler'
+
+		subcommand = @argv[0].to_s.dup
+		# Compatibility with version <= 4.0.29: accept both
+		# 'subcommand' and '--subcommand'.
+		subcommand = "--#{subcommand}" if subcommand !~ /^--/
+
+		case subcommand
+		when "--root"
+			puts PhusionPassenger.source_root
+		when "--includedir"
+			puts PhusionPassenger.include_dir
+		when "--nginx-addon-dir"
+			puts PhusionPassenger.nginx_module_source_dir
+		when "--nginx-libs"
+			puts "#{common_library.link_objects_as_string} #{PhusionPassenger.lib_dir}/common/libboost_oxt.a"
+		when "--compiled"
+			common_library.link_objects.each do |filename|
+				if !File.exist?(filename)
+					exit 1
+				end
+			end
+			if File.exist?("#{PhusionPassenger.lib_dir}/common/libboost_oxt.a")
+				exit 0
+			else
+				exit 1
+			end
+		when "--natively-packaged"
+			if PhusionPassenger.natively_packaged?
+				exit 0
+			else
+				exit 1
+			end
+		when "--installed-from-release-package"
+			if PhusionPassenger.installed_from_release_package?
+				exit 0
+			else
+				exit 1
+			end
+		when "--make-locations-ini"
+			puts "[locations]"
+			puts "natively_packaged=#{PhusionPassenger.natively_packaged?}"
+			if PhusionPassenger.natively_packaged?
+				puts "native_packaging_method=#{PhusionPassenger.native_packaging_method}"
+			end
+			PhusionPassenger::REQUIRED_LOCATIONS_INI_FIELDS.each do |field|
+				puts "#{field}=#{PhusionPassenger.send(field)}"
+			end
+			PhusionPassenger::OPTIONAL_LOCATIONS_INI_FIELDS.each do |field|
+				if value = PhusionPassenger.send(field)
+					puts "#{field}=#{value}"
+				end
+			end
+		when "--detect-apache2"
+			PhusionPassenger.require_passenger_lib 'platform_info/apache_detector'
+			detector = PhusionPassenger::PlatformInfo::ApacheDetector.new(STDOUT)
+			begin
+				detector.detect_all
+				detector.report
+			ensure
+				detector.finish
+			end
+		when "--ruby-command"
+			PhusionPassenger.require_passenger_lib 'platform_info/ruby'
+			ruby = PhusionPassenger::PlatformInfo.ruby_command
+			puts "passenger-config was invoked through the following Ruby interpreter:"
+			puts "  Command: #{ruby}"
+			STDOUT.write "  Version: "
+			STDOUT.flush
+			system("/bin/sh -c '#{ruby} -v'")
+			puts "  To use in Apache: PassengerRuby #{ruby}"
+			puts "  To use in Nginx : passenger_ruby #{ruby}"
+			puts "  To use with Standalone: #{ruby} #{PhusionPassenger.bin_dir}/passenger start"
+			puts
+
+			ruby = PhusionPassenger::PlatformInfo.find_command('ruby')
+			if ruby && !ruby.include?("rvm/rubies/")
+				# If this is an RVM Ruby executable then we don't show it. We want people to
+				# use the RVM wrapper scripts only.
+				puts "The following Ruby interpreter was found first in $PATH:"
+				puts "  Command: #{ruby}"
+				STDOUT.write "  Version: "
+				STDOUT.flush
+				system("/bin/sh -c '#{ruby} -v'")
+				puts "  To use in Apache: PassengerRuby #{ruby}"
+				puts "  To use in Nginx : passenger_ruby #{ruby}"
+				puts "  To use with Standalone: #{ruby} #{PhusionPassenger.bin_dir}/passenger start"
+			elsif !ruby.include?("rvm/rubies/")
+				puts "No Ruby interpreter found in $PATH."
+			end
+			puts
+			puts "## Notes for RVM users"
+			puts "Do you want to know which command to use for a different Ruby interpreter? 'rvm use' that Ruby interpreter, then re-run 'passenger-config --ruby-command'."
+		when "--ruby-libdir"
+			puts PhusionPassenger.ruby_libdir
+		when "--rubyext-compat-id"
+			PhusionPassenger.require_passenger_lib 'platform_info/binary_compatibility'
+			puts PhusionPassenger::PlatformInfo.ruby_extension_binary_compatibility_id
+		when "--cxx-compat-id"
+			PhusionPassenger.require_passenger_lib 'platform_info/binary_compatibility'
+			puts PhusionPassenger::PlatformInfo.cxx_binary_compatibility_id
+		when "--version"
+			puts PhusionPassenger::VERSION_STRING
+		else
+			self.class.help
+			exit 1
+		end
+	end
+
+private
+	def common_library
+		PhusionPassenger.require_passenger_lib 'common_library'
+		return COMMON_LIBRARY.
+			only(*NGINX_LIBS_SELECTOR).
+			set_output_dir("#{PhusionPassenger.lib_dir}/common/libpassenger_common")
+	end
+end
+
+end # module Config
+end # module PhusionPassenger
