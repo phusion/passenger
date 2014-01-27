dd55461bef2b5eac8e935c90a95aaa7a4c328f83
Hongli Lai (Phusion) <hongli@phusion.nl>
2014-01-27 22:16:06 +0100

    Fix Meteor WebSocket issues.
    Correct handle 'Connection: upgrade' response headers.

--- ext/common/agents/HelperAgent/RequestHandler.h
+++ ext/common/agents/HelperAgent/RequestHandler.h
@@ -2200,7 +2200,6 @@ private:
 			data.append(" ");
 			data.append(parser.getHeader("REQUEST_URI"));
 			data.append(" HTTP/1.1\r\n");
-			data.append("Connection: close\r\n");
 
 			for (it = parser.begin(); it != end; it++) {
 				if (startsWith(it->first, "HTTP_") && it->first != "HTTP_CONNECTION") {
@@ -2219,6 +2218,15 @@ private:
 					data.append(it->second);
 					data.append("\r\n");
 				}
+			}
+
+			StaticString connection = parser.getHeader("HTTP_CONNECTION");
+			if (connection == "upgrade" || connection == "Upgrade") {
+				data.append("Connection: ");
+				data.append(connection.data(), connection.size());
+				data.append("\r\n");
+			} else {
+				data.append("Connection: close\r\n");
 			}
 
 			StaticString header = parser.getHeader("CONTENT_LENGTH");
