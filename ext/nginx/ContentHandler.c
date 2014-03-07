dac809000bfdcd15a1dad9edcd30bc708b73ea0f
Hongli Lai (Phusion) <hongli@phusion.nl>
2014-03-07 13:42:05 +0100

    Fix compilation problem in Nginx module

--- ext/nginx/ContentHandler.c
+++ ext/nginx/ContentHandler.c
@@ -317,7 +317,7 @@ header_is_transfer_encoding(ngx_str_t *key)
     return key->len == sizeof("transfer-encoding") - 1 &&
         ngx_tolower(key->data[0]) == (u_char) 't' &&
         ngx_tolower(key->data[sizeof("transfer-encoding") - 2]) == (u_char) 'g' &&
-        ngx_strncasecmp(key->data + 1, "ransfer-encodin", sizeof("ransfer-encodin") - 1) == 0;
+        ngx_strncasecmp(key->data + 1, (u_char *) "ransfer-encodin", sizeof("ransfer-encodin") - 1) == 0;
 }
 
 
