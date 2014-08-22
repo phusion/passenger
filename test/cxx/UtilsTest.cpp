d82a49de8bc05aee0912214ff81cf716863d8672
Hongli Lai (Phusion) <hongli@phusion.nl>
2014-08-22 16:40:53 +0200

    Remove trailing whitespaces from some files

--- test/cxx/UtilsTest.cpp
+++ test/cxx/UtilsTest.cpp
@@ -18,31 +18,31 @@ namespace tut {
 		string cwd;
 		string oldPath;
 		TempDir tempDir;
-		
+
 		UtilsTest(): tempDir("tmp.dir") {
 			char buffer[PATH_MAX];
 			cwd = getcwd(buffer, sizeof(buffer));
 			oldPath = getenv("PATH");
 			unsetenv("PASSENGER_TEMP_DIR");
 		}
-		
+
 		~UtilsTest() {
 			setenv("PATH", oldPath.c_str(), 1);
 			unsetenv("PASSENGER_TEMP_DIR");
 			chdir(cwd.c_str());
 		}
-		
+
 		void testMakeDirTreeMode(const char *name, const char *mode, mode_t expected) {
 			TempDir td("tmp.dir2");
 			struct stat buf;
 			mode_t allModes = S_IRWXU | S_ISUID | S_IRWXG | S_ISGID | S_IRWXO;
-			
+
 			makeDirTree("tmp.dir2/foo", mode);
 			stat("tmp.dir2/foo", &buf);
 			ensure_equals(name, buf.st_mode & allModes, expected);
 		}
 	};
-	
+
 	DEFINE_TEST_GROUP_WITH_LIMIT(UtilsTest, 100);
 
 	/***** Test split() *****/
@@ -54,7 +54,7 @@ namespace tut {
 		splitIncludeSep("", ':', output);
 		ensure_equals(output.size(), 0u);
 	}
-	
+
 	TEST_METHOD(2) {
 		split("hello world", ':', output);
 		ensure_equals(output.size(), 1u);
@@ -64,7 +64,7 @@ namespace tut {
 		ensure_equals(output.size(), 1u);
 		ensure_equals(output[0], "hello world");
 	}
-	
+
 	TEST_METHOD(3) {
 		split("hello world:foo bar", ':', output);
 		ensure_equals(output.size(), 2u);
@@ -76,7 +76,7 @@ namespace tut {
 		ensure_equals(output[0], "hello world:");
 		ensure_equals(output[1], "foo bar");
 	}
-	
+
 	TEST_METHOD(4) {
 		split("hello world:", ':', output);
 		ensure_equals("(1)", output.size(), 2u);
@@ -87,7 +87,7 @@ namespace tut {
 		ensure_equals("(4)", output.size(), 1u);
 		ensure_equals("(5)", output[0], "hello world:");
 	}
-	
+
 	TEST_METHOD(5) {
 		split(":hello world", ':', output);
 		ensure_equals(output.size(), 2u);
@@ -99,7 +99,7 @@ namespace tut {
 		ensure_equals(output[0], ":");
 		ensure_equals(output[1], "hello world");
 	}
-	
+
 	TEST_METHOD(6) {
 		split("abc:def::ghi", ':', output);
 		ensure_equals(output.size(), 4u);
@@ -115,7 +115,7 @@ namespace tut {
 		ensure_equals(output[2], ":");
 		ensure_equals(output[3], "ghi");
 	}
-	
+
 	TEST_METHOD(7) {
 		split("abc:::def", ':', output);
 		ensure_equals(output.size(), 4u);
@@ -131,36 +131,36 @@ namespace tut {
 		ensure_equals(output[2], ":");
 		ensure_equals(output[3], "def");
 	}
-	
-	
+
+
 	/***** Test getSystemTempDir() *****/
-	
+
 	TEST_METHOD(11) {
 		// It returns "/tmp" if the PASSENGER_TEMP_DIR environment is NULL.
 		ensure_equals(string(getSystemTempDir()), "/tmp");
 	}
-	
+
 	TEST_METHOD(12) {
 		// It returns "/tmp" if the PASSENGER_TEMP_DIR environment is an empty string.
 		setenv("PASSENGER_TEMP_DIR", "", 1);
 		ensure_equals(string(getSystemTempDir()), "/tmp");
 	}
-	
+
 	TEST_METHOD(13) {
 		// It returns the value of the PASSENGER_TEMP_DIR environment if it is not NULL and not empty.
 		setenv("PASSENGER_TEMP_DIR", "/foo", 1);
 		ensure_equals(string(getSystemTempDir()), "/foo");
 	}
-	
-	
+
+
 	/***** Test BufferedUpload *****/
-	
+
 	TEST_METHOD(20) {
 		// The resulting file handle is readable and writable.
 		TempDir td("utils_test.tmp");
 		BufferedUpload t("utils_test.tmp");
 		char line[30];
-		
+
 		fprintf(t.handle, "hello world!");
 		fflush(t.handle);
 		fseek(t.handle, 0, SEEK_SET);
@@ -168,7 +168,7 @@ namespace tut {
 		fgets(line, sizeof(line), t.handle);
 		ensure_equals(string(line), "hello world!");
 	}
-	
+
 	TEST_METHOD(21) {
 		// It immediately unlinks the temp file.
 		DONT_RUN_IN_VAGRANT();
@@ -176,9 +176,9 @@ namespace tut {
 		BufferedUpload t("utils_test.tmp");
 		ensure_equals(listDir("utils_test.tmp").size(), 0u);
 	}
-	
+
 	/***** Test escapeForXml() *****/
-	
+
 	TEST_METHOD(25) {
 		ensure_equals(escapeForXml(""), "");
 		ensure_equals(escapeForXml("hello world"), "hello world");
@@ -188,9 +188,9 @@ namespace tut {
 		ensure_equals(escapeForXml("hello\xFF\xCCworld"), "hello&#255;&#204;world");
 		ensure_equals(escapeForXml("hello\xFFworld\xCC"), "hello&#255;world&#204;");
 	}
-	
+
 	/***** Test extractDirName() *****/
-	
+
 	TEST_METHOD(26) {
 		ensure_equals("Test 1", extractDirName("/usr/lib"), "/usr");
 		ensure_equals("Test 2", extractDirName("/usr/lib/"), "/usr");
@@ -222,9 +222,9 @@ namespace tut {
 		ensure_equals("Test 12", extractDirNameStatic(".///"), ".");
 		ensure_equals("Test 13", extractDirNameStatic("foo//bar"), "foo");
 	}
-	
+
 	/***** Test resolveSymlink() *****/
-	
+
 	TEST_METHOD(28) {
 		TempDir d("tmp.symlinks");
 		runShellCommand("touch tmp.symlinks/foo.txt");
@@ -237,41 +237,41 @@ namespace tut {
 		ensure_equals(resolveSymlink("tmp.symlinks/file3"), "tmp.symlinks/file2");
 		ensure_equals(resolveSymlink("tmp.symlinks/absolute_symlink"), "/usr/bin");
 	}
-	
+
 	/***** Test fillInMiddle() *****/
-	
+
 	TEST_METHOD(30) {
 		ensure_equals(fillInMiddle(20, "server.", "123456", ".socket"), "server.123456.socket");
 		ensure_equals(fillInMiddle(25, "server.", "123456", ".socket"), "server.123456.socket");
 		ensure_equals(fillInMiddle(19, "server.", "123456", ".socket"), "server.12345.socket");
 		ensure_equals(fillInMiddle(16, "server.", "123456", ".socket"), "server.12.socket");
-		
+
 		ensure_equals(fillInMiddle(10, "", "1234", ""), "1234");
 		ensure_equals(fillInMiddle(4, "", "1234", ""), "1234");
 		ensure_equals(fillInMiddle(2, "", "1234", ""), "12");
-		
+
 		ensure_equals(fillInMiddle(20, "", "1234", ".socket"), "1234.socket");
 		ensure_equals(fillInMiddle(11, "", "1234", ".socket"), "1234.socket");
 		ensure_equals(fillInMiddle(9, "", "1234", ".socket"), "12.socket");
-		
+
 		try {
 			fillInMiddle(14, "server.", "123456", ".socket");
 			fail();
 		} catch (const ArgumentException &) { }
-		
+
 		try {
 			fillInMiddle(10, "server.", "123456", ".socket");
 			fail();
 		} catch (const ArgumentException &) { }
-		
+
 		try {
 			fillInMiddle(10, "server.", "", ".socket");
 			fail();
 		} catch (const ArgumentException &) { }
 	}
-	
+
 	/***** Test MemZeroGuard *****/
-	
+
 	TEST_METHOD(31) {
 		char buf[12] = "hello world";
 		{
@@ -279,7 +279,7 @@ namespace tut {
 		}
 		ensure(memcmp(buf, "\0\0llo world", sizeof(buf)) == 0);
 	}
-	
+
 	TEST_METHOD(32) {
 		string str("hello ");
 		{
@@ -288,7 +288,7 @@ namespace tut {
 		}
 		ensure(memcmp(str.c_str(), "\0\0\0\0\0\0\0\0\0\0\0", 11) == 0);
 	}
-	
+
 	TEST_METHOD(33) {
 		string str("hello ");
 		{
@@ -300,9 +300,9 @@ namespace tut {
 		}
 		ensure(memcmp(str.c_str(), "\0\0\0\0\0\0\0\0\0\0\0", 11) == 0);
 	}
-	
+
 	/***** Test parseModeString() *****/
-	
+
 	static bool modeStringCannotBeParsed(const StaticString &modeString) {
 		try {
 			parseModeString(modeString);
@@ -311,7 +311,7 @@ namespace tut {
 			return true;
 		}
 	}
-	
+
 	TEST_METHOD(36) {
 		ensure_equals(parseModeString(""), (mode_t) 0);
 		ensure_equals(parseModeString("u="), (mode_t) 0);
@@ -321,7 +321,7 @@ namespace tut {
 		ensure_equals(parseModeString("u=,g=,o=,u=,g="), (mode_t) 0);
 		ensure_equals(parseModeString("o="), (mode_t) 0);
 	}
-	
+
 	TEST_METHOD(37) {
 		ensure_equals("(1)", parseModeString("u=rwx"), (mode_t) S_IRWXU);
 		ensure_equals("(2)", parseModeString("g=rwx"), (mode_t) S_IRWXG);
@@ -337,7 +337,7 @@ namespace tut {
 		ensure_equals("(7)", parseModeString("u=rwx,g=rwx,+t"),
 			(mode_t) (S_IRWXU | S_IRWXG | S_ISVTX));
 	}
-	
+
 	TEST_METHOD(38) {
 		ensure(modeStringCannotBeParsed("0"));
 		ensure(modeStringCannotBeParsed("0600"));
@@ -347,31 +347,31 @@ namespace tut {
 		ensure(modeStringCannotBeParsed("x=rs"));
 		ensure(modeStringCannotBeParsed("rwxrwxrwx"));
 	}
-	
+
 	/***** Test makeDirTree() *****/
-	
+
 	TEST_METHOD(40) {
 		// Creating a single subdirectory works.
 		makeDirTree("tmp.dir/foo");
 		ensure_equals(getFileType("tmp.dir/foo"), FT_DIRECTORY);
 	}
-	
+
 	TEST_METHOD(41) {
 		// Creating multiple subdirectories works.
 		makeDirTree("tmp.dir/foo/bar");
 		ensure_equals(getFileType("tmp.dir/foo"), FT_DIRECTORY);
 		ensure_equals(getFileType("tmp.dir/foo/bar"), FT_DIRECTORY);
 	}
-	
+
 	TEST_METHOD(42) {
 		// It applies the permissions to all created directories.
 		struct stat buf, buf2;
-		
+
 		stat("tmp.dir", &buf);
 		makeDirTree("tmp.dir/foo/bar", "u=rwxs,g=,o=rx");
 		stat("tmp.dir", &buf2);
 		ensure_equals(buf.st_mode, buf2.st_mode);
-		
+
 		stat("tmp.dir/foo", &buf);
 		stat("tmp.dir/foo/bar", &buf2);
 		ensure_equals(buf.st_mode, buf2.st_mode);
@@ -379,7 +379,7 @@ namespace tut {
 			(mode_t) (S_IRUSR | S_IWUSR | S_IXUSR | S_ISUID |
 			S_IROTH | S_IXOTH));
 	}
-	
+
 	TEST_METHOD(43) {
 		// It correctly parses the permission string.
 		testMakeDirTreeMode("empty 1", "", (mode_t) 0);
@@ -388,7 +388,7 @@ namespace tut {
 		testMakeDirTreeMode("empty 4", "o=", (mode_t) 0);
 		testMakeDirTreeMode("empty 5", "u=,g=", (mode_t) 0);
 		testMakeDirTreeMode("empty 6", "g=,o=", (mode_t) 0);
-		
+
 		testMakeDirTreeMode("(1)", "u=rwxs,g=rwxs,o=rwx",
 			S_IRWXU | S_ISUID | S_IRWXG | S_ISGID | S_IRWXO);
 		testMakeDirTreeMode("(2)", "u=s,g=rx,o=w",
@@ -396,7 +396,7 @@ namespace tut {
 		testMakeDirTreeMode("(3)", "u=rwxs,g=,o=rwx",
 			S_IRWXU | S_ISUID | S_IRWXO);
 	}
-	
+
 	TEST_METHOD(44) {
 		// It doesn't do anything if the directory already exists.
 		struct stat buf, buf2;
@@ -405,9 +405,9 @@ namespace tut {
 		stat("tmp.dir", &buf2);
 		ensure_equals(buf.st_mode, buf2.st_mode);
 	}
-	
+
 	/***** Test stringToULL(), stringToUint *****/
-	
+
 	TEST_METHOD(47) {
 		ensure_equals(stringToULL(""), 0ull);
 		ensure_equals(stringToULL("bla"), 0ull);
@@ -420,7 +420,7 @@ namespace tut {
 		ensure_equals(stringToULL("2937104"), 2937104ull);
 		ensure_equals(stringToULL("18446744073709551615"), 18446744073709551615ull);
 		ensure_equals(stringToULL("    5abcdef1234"), 5ull);
-		
+
 		ensure_equals(stringToUint(""), 0u);
 		ensure_equals(stringToUint("bla"), 0u);
 		ensure_equals(stringToUint("0"), 0u);
@@ -432,55 +432,55 @@ namespace tut {
 		ensure_equals(stringToUint("2937104"), 2937104u);
 		ensure_equals(stringToUint("    5abcdef1234"), 5u);
 	}
-	
+
 	/***** Test integerToHex() and integerToHexatri() *****/
-	
+
 	TEST_METHOD(48) {
 		char buf[sizeof(int) * 2 + 1];
-		
+
 		ensure_equals("(1)", integerToHex<int>(0x0, buf), 1u);
 		ensure("(1)", strcmp(buf, "0") == 0);
-		
+
 		ensure_equals("(2)", integerToHex<int>(0x1, buf), 1u);
 		ensure("(2)", strcmp(buf, "1") == 0);
-		
+
 		ensure_equals("(3)", integerToHex<int>(0x9, buf), 1u);
 		ensure("(3)", strcmp(buf, "9") == 0);
-		
+
 		ensure_equals("(4)", integerToHex<int>(0xe, buf), 1u);
 		ensure("(4)", strcmp(buf, "e") == 0);
-		
+
 		ensure_equals("(5)", integerToHex<unsigned int>(0xdeadbeef, buf), 8u);
 		ensure("(5)", strcmp(buf, "deadbeef") == 0);
-		
+
 		ensure_equals("(6)", integerToHex<int>(0x1234f, buf), 5u);
 		ensure("(6)", strcmp(buf, "1234f") == 0);
-		
-		
+
+
 		ensure_equals("(7)", integerToHexatri<int>(0x0, buf), 1u);
 		ensure("(7)", strcmp(buf, "0") == 0);
-		
+
 		ensure_equals("(8)", integerToHexatri<int>(0x1, buf), 1u);
 		ensure("(8)", strcmp(buf, "1") == 0);
-		
+
 		ensure_equals("(9)", integerToHexatri<int>(0x9, buf), 1u);
 		ensure("(9)", strcmp(buf, "9") == 0);
-		
+
 		ensure_equals("(10)", integerToHexatri<int>(0xe, buf), 1u);
 		ensure("(10)", strcmp(buf, "e") == 0);
-		
+
 		ensure_equals("(11)", integerToHexatri<int>(35, buf), 1u);
 		ensure("(11)", strcmp(buf, "z") == 0);
-		
+
 		ensure_equals(integerToHexatri<unsigned int>(0xdeadbeef, buf), 7u);
 		ensure(strcmp(buf, "1ps9wxb") == 0);
-		
+
 		ensure_equals(integerToHexatri<int>(0x1234f, buf), 4u);
 		ensure(strcmp(buf, "1ljj") == 0);
 	}
-	
+
 	/***** Test hexToULL(), hexToUint() and hexatriToULL() *****/
-	
+
 	TEST_METHOD(49) {
 		ensure_equals(hexToULL(""), 0ull);
 		ensure_equals(hexToULL("   "), 0ull);
@@ -496,7 +496,7 @@ namespace tut {
 		ensure_equals(hexToULL("dEaDbEeF"), 3735928559ull);
 		ensure_equals(hexToULL("09a2s89"), 2466ull);
 		ensure_equals(hexToULL(" 9a2s89"), 0ull);
-		
+
 		ensure_equals(hexToUint(""), 0u);
 		ensure_equals(hexToUint("   "), 0u);
 		ensure_equals(hexToUint("1"), 1u);
@@ -511,7 +511,7 @@ namespace tut {
 		ensure_equals(hexToUint("dEaDbEeF"), 3735928559u);
 		ensure_equals(hexToUint("09a2s89"), 2466u);
 		ensure_equals(hexToUint(" 9a2s89"), 0u);
-		
+
 		ensure_equals(hexatriToULL(""), 0ull);
 		ensure_equals(hexatriToULL("   "), 0ull);
 		ensure_equals(hexatriToULL("1"), 1ull);
@@ -527,9 +527,9 @@ namespace tut {
 		ensure_equals(hexatriToULL("09a2s89"), 561121641ull);
 		ensure_equals(hexatriToULL(" 9a2s89"), 0ull);
 	}
-	
+
 	/***** Test stringToLL(), stringToInt() *****/
-	
+
 	TEST_METHOD(50) {
 		ensure_equals(stringToLL(""), 0ll);
 		ensure_equals(stringToLL("bla"), 0ll);
@@ -542,14 +542,14 @@ namespace tut {
 		ensure_equals(stringToLL("2937104"), 2937104ll);
 		ensure_equals(stringToLL("9223372036854775807"), 9223372036854775807ll);
 		ensure_equals(stringToLL("    5abcdef1234"), 5ll);
-		
+
 		ensure_equals(stringToLL("-0"), 0ll);
 		ensure_equals(stringToLL("-1"), -1ll);
 		ensure_equals(stringToLL("-010"), -10ll);
 		ensure_equals(stringToLL("-9876"), -9876ll);
 		ensure_equals(stringToLL("-9223372036854775807"), -9223372036854775807ll);
 		ensure_equals(stringToLL("    -5abcdef1234"), -5ll);
-		
+
 		ensure_equals(stringToInt(""), 0);
 		ensure_equals(stringToInt("bla"), 0);
 		ensure_equals(stringToInt("0"), 0);
@@ -560,16 +560,16 @@ namespace tut {
 		ensure_equals(stringToInt("928"), 928);
 		ensure_equals(stringToInt("2937104"), 2937104);
 		ensure_equals(stringToInt("    5abcdef1234"), 5);
-		
+
 		ensure_equals(stringToInt("-0"), 0ll);
 		ensure_equals(stringToInt("-1"), -1ll);
 		ensure_equals(stringToInt("-010"), -10ll);
 		ensure_equals(stringToInt("-9876"), -9876);
 		ensure_equals(stringToInt("    -5abcdef1234"), -5);
 	}
-	
+
 	/***** Test cEscapeString() *****/
-	
+
 	TEST_METHOD(51) {
 		ensure_equals(cEscapeString(""), "");
 		ensure_equals(cEscapeString("abcdXYZ123!?"), "abcdXYZ123!?");
@@ -579,9 +579,9 @@ namespace tut {
 			"\\x00\\x01\\x02\\x03\\x04\\x05\\x06\\x07\\x08\\t");
 		ensure_equals(cEscapeString("\xFF\xFE\t\xD0"), "\\xFF\\xFE\\t\\xD0");
 	}
-	
+
 	/***** Test escapeHTML() *****/
-	
+
 	TEST_METHOD(52) {
 		const char weird[] = "Weird \x01\x00 characters?";
 		ensure_equals(escapeHTML(""), "");
@@ -655,7 +655,7 @@ namespace tut {
 		ensure("(7)", !constantTimeCompare("ab", "cd"));
 		ensure("(8)", !constantTimeCompare("ab", "abc"));
 		ensure("(9)", !constantTimeCompare("ab", "abcd"));
-		
+
 		ensure("(10)", !constantTimeCompare("a", ""));
 		ensure("(11)", !constantTimeCompare("abcd", ""));
 		ensure("(12)", !constantTimeCompare("cd", "ab"));
