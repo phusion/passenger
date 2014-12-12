7bbd9189a06f0944cf05a738e66f94c6238065b1
Hongli Lai (Phusion) <hongli@phusion.nl>
2014-12-12 15:03:36 +0100

    Bump version to 4.0.55, update changelog

--- lib/phusion_passenger.rb
+++ lib/phusion_passenger.rb
@@ -30,7 +30,7 @@ module PhusionPassenger
 
 	PACKAGE_NAME = 'passenger'
 	# Run 'rake ext/common/Constants.h' after changing this number.
-	VERSION_STRING = '4.0.54'
+	VERSION_STRING = '4.0.55'
 
 	PREFERRED_NGINX_VERSION = '1.6.2'
 	NGINX_SHA256_CHECKSUM = 'b5608c2959d3e7ad09b20fc8f9e5bd4bc87b3bc8ba5936a513c04ed8f1391a18'
