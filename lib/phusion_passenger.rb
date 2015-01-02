f8ad08c5b7f5ba5be318aab5260b3b7ac8bc2d5b
Hongli Lai (Phusion) <hongli@phusion.nl>
2015-01-02 23:16:08 +0100

    Bump version to 4.0.57

--- lib/phusion_passenger.rb
+++ lib/phusion_passenger.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 #  Phusion Passenger - https://www.phusionpassenger.com/
-#  Copyright (c) 2010-2014 Phusion
+#  Copyright (c) 2010-2015 Phusion
 #
 #  "Phusion Passenger" is a trademark of Hongli Lai & Ninh Bui.
 #
@@ -30,7 +30,7 @@ module PhusionPassenger
 
 	PACKAGE_NAME = 'passenger'
 	# Run 'rake ext/common/Constants.h' after changing this number.
-	VERSION_STRING = '4.0.56'
+	VERSION_STRING = '4.0.57'
 
 	PREFERRED_NGINX_VERSION = '1.6.2'
 	NGINX_SHA256_CHECKSUM = 'b5608c2959d3e7ad09b20fc8f9e5bd4bc87b3bc8ba5936a513c04ed8f1391a18'
