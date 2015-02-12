738675b1ff6032875ea7df108ab415748106767f
Hongli Lai (Phusion) <hongli@phusion.nl>
2015-02-12 16:19:34 +0100

    Use standard Ruby coding style for vendored daemon_controller library

--- lib/phusion_passenger/vendor/daemon_controller/lock_file.rb
+++ lib/phusion_passenger/vendor/daemon_controller/lock_file.rb
@@ -1,5 +1,5 @@
 # daemon_controller, library for robust daemon management
-# Copyright (c) 2010 Phusion
+# Copyright (c) 2010-2015 Phusion
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -22,109 +22,106 @@
 require 'fcntl'
 
 module PhusionPassenger
-
 class DaemonController
+  # A lock file is a synchronization mechanism, like a Mutex, but it also allows
+  # inter-process synchronization (as opposed to only inter-thread synchronization
+  # within a single process).
+  #
+  # Processes can obtain either a shared lock or an exclusive lock. It's possible
+  # for multiple processes to obtain a shared lock on a file as long as no
+  # exclusive lock has been obtained by a process. If a process has obtained an
+  # exclusive lock, then no other processes can lock the file, whether they're
+  # trying to obtain a shared lock or an exclusive lock.
+  #
+  # Note that on JRuby, LockFile can only guarantee synchronization between
+  # threads if the different threads use the same LockFile object. Specifying the
+  # same filename is not enough.
+  class LockFile
+    class AlreadyLocked < StandardError
+    end
 
-# A lock file is a synchronization mechanism, like a Mutex, but it also allows
-# inter-process synchronization (as opposed to only inter-thread synchronization
-# within a single process).
-#
-# Processes can obtain either a shared lock or an exclusive lock. It's possible
-# for multiple processes to obtain a shared lock on a file as long as no
-# exclusive lock has been obtained by a process. If a process has obtained an
-# exclusive lock, then no other processes can lock the file, whether they're
-# trying to obtain a shared lock or an exclusive lock.
-#
-# Note that on JRuby, LockFile can only guarantee synchronization between
-# threads if the different threads use the same LockFile object. Specifying the
-# same filename is not enough.
-class LockFile
-	class AlreadyLocked < StandardError
-	end
+    # Create a LockFile object. The lock file is initially not locked.
+    #
+    # +filename+ may point to a nonexistant file. In that case, the lock
+    # file will not be created until one's trying to obtain a lock.
+    #
+    # Note that LockFile will use this exact filename. So if +filename+
+    # is a relative filename, then the actual lock file that will be used
+    # depends on the current working directory.
+    def initialize(filename)
+      @filename = filename
+    end
 
-	# Create a LockFile object. The lock file is initially not locked.
-	#
-	# +filename+ may point to a nonexistant file. In that case, the lock
-	# file will not be created until one's trying to obtain a lock.
-	#
-	# Note that LockFile will use this exact filename. So if +filename+
-	# is a relative filename, then the actual lock file that will be used
-	# depends on the current working directory.
-	def initialize(filename)
-		@filename = filename
-	end
+    # Obtain an exclusive lock on the lock file, yield the given block,
+    # then unlock the lockfile. If the lock file was already locked (whether
+    # shared or exclusively) by another process/thread then this method will
+    # block until the lock file has been unlocked.
+    #
+    # The lock file *must* be writable, otherwise an Errno::EACCESS
+    # exception will be raised.
+    def exclusive_lock
+      File.open(@filename, 'w') do |f|
+        if Fcntl.const_defined? :F_SETFD
+          f.fcntl(Fcntl::F_SETFD, Fcntl::FD_CLOEXEC)
+        end
+        f.flock(File::LOCK_EX)
+        yield
+      end
+    end
 
-	# Obtain an exclusive lock on the lock file, yield the given block,
-	# then unlock the lockfile. If the lock file was already locked (whether
-	# shared or exclusively) by another process/thread then this method will
-	# block until the lock file has been unlocked.
-	#
-	# The lock file *must* be writable, otherwise an Errno::EACCESS
-	# exception will be raised.
-	def exclusive_lock
-		File.open(@filename, 'w') do |f|
-			if Fcntl.const_defined? :F_SETFD
-				f.fcntl(Fcntl::F_SETFD, Fcntl::FD_CLOEXEC)
-			end
-			f.flock(File::LOCK_EX)
-			yield
-		end
-	end
+    # Obtain an exclusive lock on the lock file, yield the given block,
+    # then unlock the lockfile. If the lock file was already exclusively
+    # locked by another process/thread then this method will
+    # block until the exclusive lock has been released. This method will not
+    # block if only shared locks have been obtained.
+    #
+    # The lock file *must* be writable, otherwise an Errno::EACCESS
+    # exception will be raised.
+    def shared_lock
+      File.open(@filename, 'w+') do |f|
+        if Fcntl.const_defined? :F_SETFD
+          f.fcntl(Fcntl::F_SETFD, Fcntl::FD_CLOEXEC)
+        end
+        f.flock(File::LOCK_SH)
+        yield
+      end
+    end
 
-	# Obtain an exclusive lock on the lock file, yield the given block,
-	# then unlock the lockfile. If the lock file was already exclusively
-	# locked by another process/thread then this method will
-	# block until the exclusive lock has been released. This method will not
-	# block if only shared locks have been obtained.
-	#
-	# The lock file *must* be writable, otherwise an Errno::EACCESS
-	# exception will be raised.
-	def shared_lock
-		File.open(@filename, 'w+') do |f|
-			if Fcntl.const_defined? :F_SETFD
-				f.fcntl(Fcntl::F_SETFD, Fcntl::FD_CLOEXEC)
-			end
-			f.flock(File::LOCK_SH)
-			yield
-		end
-	end
+    # Try to obtain a shared lock on the lock file, similar to #shared_lock.
+    # But unlike #shared_lock, this method will raise AlreadyLocked if
+    # no lock can be obtained, instead of blocking.
+    #
+    # If a lock can be obtained, then the given block will be yielded.
+    def try_shared_lock
+      File.open(@filename, 'w+') do |f|
+        if Fcntl.const_defined? :F_SETFD
+          f.fcntl(Fcntl::F_SETFD, Fcntl::FD_CLOEXEC)
+        end
+        if f.flock(File::LOCK_SH | File::LOCK_NB)
+          yield
+        else
+          raise AlreadyLocked
+        end
+      end
+    end
 
-	# Try to obtain a shared lock on the lock file, similar to #shared_lock.
-	# But unlike #shared_lock, this method will raise AlreadyLocked if
-	# no lock can be obtained, instead of blocking.
-	#
-	# If a lock can be obtained, then the given block will be yielded.
-	def try_shared_lock
-		File.open(@filename, 'w+') do |f|
-			if Fcntl.const_defined? :F_SETFD
-				f.fcntl(Fcntl::F_SETFD, Fcntl::FD_CLOEXEC)
-			end
-			if f.flock(File::LOCK_SH | File::LOCK_NB)
-				yield
-			else
-				raise AlreadyLocked
-			end
-		end
-	end
-
-	# Try to obtain an exclusive lock on the lock file, similar to #exclusive_lock.
-	# But unlike #exclusive_lock, this method will raise AlreadyLocked if
-	# no lock can be obtained, instead of blocking.
-	#
-	# If a lock can be obtained, then the given block will be yielded.
-	def try_exclusive_lock
-		File.open(@filename, 'w') do |f|
-			if Fcntl.const_defined? :F_SETFD
-				f.fcntl(Fcntl::F_SETFD, Fcntl::FD_CLOEXEC)
-			end
-			if f.flock(File::LOCK_EX | File::LOCK_NB)
-				yield
-			else
-				raise AlreadyLocked
-			end
-		end
-	end
-end # class LockFile
+    # Try to obtain an exclusive lock on the lock file, similar to #exclusive_lock.
+    # But unlike #exclusive_lock, this method will raise AlreadyLocked if
+    # no lock can be obtained, instead of blocking.
+    #
+    # If a lock can be obtained, then the given block will be yielded.
+    def try_exclusive_lock
+      File.open(@filename, 'w') do |f|
+        if Fcntl.const_defined? :F_SETFD
+          f.fcntl(Fcntl::F_SETFD, Fcntl::FD_CLOEXEC)
+        end
+        if f.flock(File::LOCK_EX | File::LOCK_NB)
+          yield
+        else
+          raise AlreadyLocked
+        end
+      end
+    end
+  end # class LockFile
 end # class DaemonController
-
 end # module PhusionPassenger
--- lib/phusion_passenger/vendor/daemon_controller/version.rb
+++ lib/phusion_passenger/vendor/daemon_controller/version.rb
@@ -1,5 +1,5 @@
 # daemon_controller, library for robust daemon management
-# Copyright (c) 2010-2014 Phusion
+# Copyright (c) 2010-2015 Phusion
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -20,12 +20,10 @@
 # THE SOFTWARE.
 
 module PhusionPassenger
-
 class DaemonController
-	MAJOR = 1
-	MINOR = 2
-	TINY  = 1
-	VERSION_STRING = "#{MAJOR}.#{MINOR}.#{TINY}"
+  MAJOR = 1
+  MINOR = 2
+  TINY  = 1
+  VERSION_STRING = "#{MAJOR}.#{MINOR}.#{TINY}"
 end # class DaemonController
-
 end # module PhusionPassenger
