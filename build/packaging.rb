3d40a28e9f82c70ee53094d87a61c605168e9159
Hongli Lai (Phusion) <hongli@phusion.nl>
2015-03-06 08:26:41 +0100

    Use ERB instead of our custom preprocessor language for preprocessing Debian package spec files
    The preprocessor didn't handle if/elif/else correctly. It's easier to use ERB instead.

--- build/packaging.rb
+++ build/packaging.rb
@@ -1,5 +1,5 @@
 #  Phusion Passenger - https://www.phusionpassenger.com/
-#  Copyright (c) 2010-2014 Phusion
+#  Copyright (c) 2010-2015 Phusion
 #
 #  "Phusion Passenger" is a trademark of Hongli Lai & Ninh Bui.
 #
@@ -34,8 +34,8 @@ def recursive_copy_files(files, destination_dir, preprocess = false, variables =
       FileUtils.mkdir_p("#{destination_dir}/#{dir}")
     end
     if !File.directory?(filename)
-      if preprocess && filename =~ /\.template$/
-        real_filename = filename.sub(/\.template$/, '')
+      if preprocess && filename =~ /\.erb$/
+        real_filename = filename.sub(/\.erb$/, '')
         FileUtils.install(filename, "#{destination_dir}/#{real_filename}", :preserve => true)
         Preprocessor.new.start(filename, "#{destination_dir}/#{real_filename}",
           variables)
--- build/preprocessor.rb
+++ build/preprocessor.rb
@@ -22,43 +22,9 @@
 #  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 #  THE SOFTWARE.
 
-# Implements a simple preprocessor language which combines elements in the C
-# preprocessor with ERB:
-#
-#     Today
-#     #if @today == :fine
-#         is a fine day.
-#     #elif @today == :good
-#         is a good day.
-#     #else
-#         is a sad day.
-#     #endif
-#     Let's go walking.
-#     Today is <%= Time.now %>.
-#
-# When run with...
-#
-#     Preprocessor.new.start('input.txt', 'output.txt', :today => :fine)
-#
-# ...will produce:
-#
-#     Today
-#     is a fine day.
-#     Let's go walking.
-#     Today is 2013-08-11 22:37:06 +0200.
-#
-# Highlights:
-#
-#  * #if blocks can be nested.
-#  * Expressions are Ruby expressions, evaluated within the binding of a
-#    Preprocessor::Evaluator object.
-#  * Text inside #if/#elif/#else are automatically unindented.
-#  * ERB compatible.
 class Preprocessor
   def initialize
     require 'erb' if !defined?(ERB)
-    @indentation_size = 4
-    @debug = boolean_option('DEBUG')
   end
 
   def start(filename, output_filename, variables = {})
@@ -69,99 +35,10 @@ class Preprocessor
       output = STDOUT
     end
     the_binding  = create_binding(variables)
-    context      = []
-    @filename    = filename
-    @lineno      = 1
-    @indentation = 0
 
-    each_line(filename, the_binding) do |line|
-      debug("context=#{context.inspect}, line=#{line.inspect}")
-
-      name, args_string, cmd_indentation = recognize_command(line)
-      case name
-      when "if"
-        case context.last
-        when nil, :if_true, :else_true
-          check_indentation(cmd_indentation)
-          result = the_binding.eval(args_string, filename, @lineno)
-          context.push(result ? :if_true : :if_false)
-          inc_indentation
-        when :if_false, :else_false, :if_ignore
-          check_indentation(cmd_indentation)
-          inc_indentation
-          context.push(:if_ignore)
-        else
-          terminate "#if is not allowed in this context"
-        end
-      when "elif"
-        case context.last
-        when :if_true
-          dec_indentation
-          check_indentation(cmd_indentation)
-          inc_indentation
-          context[-1] = :if_false
-        when :if_false
-          dec_indentation
-          check_indentation(cmd_indentation)
-          inc_indentation
-          result = the_binding.eval(args_string, filename, @lineno)
-          context[-1] = result ? :if_true : :if_false
-        when :else_true, :else_false
-          terminate "#elif is not allowed after #else"
-        when :if_ignore
-          dec_indentation
-          check_indentation(cmd_indentation)
-          inc_indentation
-        else
-          terminate "#elif is not allowed outside #if block"
-        end
-      when "else"
-        case context.last
-        when :if_true
-          dec_indentation
-          check_indentation(cmd_indentation)
-          inc_indentation
-          context[-1] = :else_false
-        when :if_false
-          dec_indentation
-          check_indentation(cmd_indentation)
-          inc_indentation
-          context[-1] = :else_true
-        when :else_true, :else_false
-          terminate "it is not allowed to have multiple #else clauses in one #if block"
-        when :if_ignore
-          dec_indentation
-          check_indentation(cmd_indentation)
-          inc_indentation
-        else
-          terminate "#else is not allowed outside #if block"
-        end
-      when "endif"
-        case context.last
-        when :if_true, :if_false, :else_true, :else_false, :if_ignore
-          dec_indentation
-          check_indentation(cmd_indentation)
-          context.pop
-        else
-          terminate "#endif is not allowed outside #if block"
-        end
-      when "DEBHELPER"
-        output.puts(line)
-      when "", nil
-        # Either a comment or not a preprocessor command.
-        case context.last
-        when nil, :if_true, :else_true
-          output.puts(unindent(line))
-        else
-          # Check indentation but do not output.
-          unindent(line)
-        end
-      else
-        terminate "Unrecognized preprocessor command ##{name.inspect}"
-      end
-
-      @lineno += 1
-    end
+    erb = ERB.new(File.read(filename), nil, "-")
+    erb.filename = filename
+    output.write(erb.result(the_binding))
   ensure
     if output_filename && output
       output.close
@@ -251,37 +128,6 @@ private
     end
   end
 
-  def each_line(filename, the_binding)
-    data = File.open(filename, 'r') do |f|
-      erb = ERB.new(f.read, nil, "-")
-      erb.filename = filename
-      erb.result(the_binding)
-    end
-    data.each_line do |line|
-      yield line.chomp
-    end
-  end
-
-  def recognize_command(line)
-    if line =~ /^([\s\t]*)#(.+)/
-      indentation_str = $1
-      command = $2
-
-      # Declare tabs as equivalent to 4 spaces. This is necessary for
-      # Makefiles in which the use of tabs is required.
-      indentation_str.gsub!("\t", "    ")
-
-      name = command.scan(/^\w+/).first
-      # Ignore shebangs and comments.
-      return if name.nil?
-
-      args_string = command.sub(/^#{Regexp.escape(name)}[\s\t]*/, '')
-      return [name, args_string, indentation_str.to_s.size]
-    else
-      return nil
-    end
-  end
-
   def create_binding(variables)
     object = Evaluator.new
     variables.each_pair do |key, val|
@@ -290,52 +136,5 @@ private
     return object.instance_eval do
       binding
     end
-  end
-
-  def inc_indentation
-    @indentation += @indentation_size
-  end
-
-  def dec_indentation
-    @indentation -= @indentation_size
-  end
-
-  def check_indentation(expected)
-    if expected != @indentation
-      terminate "wrong indentation: found #{expected} characters, should be #{@indentation}"
-    end
-  end
-
-  def unindent(line)
-    line =~ /^([\s\t]*)/
-    # Declare tabs as equivalent to 4 spaces. This is necessary for
-    # Makefiles in which the use of tabs is required.
-    found = $1.to_s.gsub("\t", "    ").size
-
-    if found >= @indentation
-      # Tab-friendly way to remove indentation.
-      remaining = @indentation
-      line = line.dup
-      while remaining > 0
-        if line[0..0] == " "
-          remaining -= 1
-        else
-          # This is a tab.
-          remaining -= 4
-        end
-        line.slice!(0, 1)
-      end
-      return line
-    else
-      terminate "wrong indentation: found #{found} characters, should be at least #{@indentation}"
-    end
-  end
-
-  def debug(message)
-    puts "DEBUG:#{@lineno}: #{message}" if @debug
-  end
-
-  def terminate(message)
-    abort "*** ERROR: #{@filename} line #{@lineno}: #{message}"
   end
 end
