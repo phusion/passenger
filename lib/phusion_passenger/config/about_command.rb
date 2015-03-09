6053f360f63f90d63e9c85f1f8fac6e3202fa21f
Hongli Lai (Phusion) <hongli@phusion.nl>
2015-03-09 10:51:41 +0100

    Show in headers and in 'passenger-config --version' whether Passenger Enterprise is active

--- lib/phusion_passenger/config/about_command.rb
+++ lib/phusion_passenger/config/about_command.rb
@@ -1,5 +1,5 @@
 #  Phusion Passenger - https://www.phusionpassenger.com/
-#  Copyright (c) 2010-2014 Phusion
+#  Copyright (c) 2010-2015 Phusion
 #
 #  "Phusion Passenger" is a trademark of Hongli Lai & Ninh Bui.
 #
@@ -175,7 +175,12 @@ module PhusionPassenger
           PhusionPassenger.require_passenger_lib 'platform_info/binary_compatibility'
           puts PhusionPassenger::PlatformInfo.cxx_binary_compatibility_id
         when "--version"
-          puts PhusionPassenger::VERSION_STRING
+          if defined?(PhusionPassenger::PASSENGER_IS_ENTERPRISE)
+            name = "#{PhusionPassenger::PROGRAM_NAME} Enterprise"
+          else
+            name = PhusionPassenger::PROGRAM_NAME
+          end
+          puts "#{name} #{PhusionPassenger::VERSION_STRING}"
         when "--help"
           self.class.help
         else
