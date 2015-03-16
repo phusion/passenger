1157a83bd88e1a4f7066da70af8e02ab6113676b
Daniel Knoppel (Phusion) <daniel@phusion.nl>
2015-03-16 20:06:36 +0100

    Workaround for Solaris compilation problem (tm_gmtoff not supported). Closes GH-1435.

--- ext/common/Utils/DateParsing.h
+++ ext/common/Utils/DateParsing.h
@@ -49,7 +49,7 @@ inline bool parseImfFixdate_zone(const char **pos, const char *end, int &zone);
 
 
 /**
- * Parses an IMF-fixdate, as defined by RFC 5322.
+ * Parses an IMF-fixdate, as defined by RFC 7231 (while referencing RFC 5322).
  * Returns whether parsing succeeded.
  */
 inline bool
@@ -79,10 +79,18 @@ parseImfFixdate(const char *date, const char *end, struct tm &tm, int &zone) {
  */
 inline time_t
 parsedDateToTimestamp(struct tm &tm, int zone) {
-	time_t result = mktime(&tm);
-	result -= zone / 100 * 60 * 60 + zone % 100 * 60;
-	result += tm.tm_gmtoff;
-	return result;
+	time_t timeUsingLocalTZ = mktime(&tm);
+
+	// tmUsingLocalTZ represents tm interpreted as being in our local timezone instead of the intended UTC.
+	// We could add tm.tm_gmtoff ("seconds east of UTC"), but that doesn't exist everywhere (e.g. Solaris)
+
+	// Alternative: let gtmtime show us the current offset by converting to UTC and pretending that is local time
+	// again, resulting in a "corrected" timestamp that we can delta with the original timestamp to get the offset.
+	struct tm *tmAsUTC = gmtime(&timeUsingLocalTZ);
+	time_t utcUsingLocalTZ = mktime(tmAsUTC);
+
+	// The final result also needs to take into account the desired zone.
+	return timeUsingLocalTZ - (zone / 100 * 60 * 60 + zone % 100 * 60) - (utcUsingLocalTZ - timeUsingLocalTZ);
 }
 
 inline void
