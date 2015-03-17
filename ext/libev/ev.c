88c9ead7bc53ceab534ede81e29fb41c42cd52ff
Hongli Lai (Phusion) <hongli@phusion.nl>
2015-03-17 22:36:45 +0100

    Fix compilation problems with Clang 3.6 on Linux

--- ext/libev/ev.c
+++ ext/libev/ev.c
@@ -967,7 +967,7 @@ ecb_inline uint64_t ecb_rotr64 (uint64_t x, unsigned int count) { return (x << (
   #define ecb_unreachable() __builtin_unreachable ()
 #else
   /* this seems to work fine, but gcc always emits a warning for it :/ */
-  ecb_inline void ecb_unreachable (void) ecb_noreturn;
+  ecb_inline ecb_noreturn void ecb_unreachable (void);
   ecb_inline void ecb_unreachable (void) { }
 #endif
 
