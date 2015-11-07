ab66b6a7bff357ab6da2396ca1f759a6fef4fa50
Hongli Lai (Phusion) <hongli@phusion.nl>
2015-11-07 10:03:54 +0100

    Passenger Standalone: fix crash if Unix domain socket is suddenly deleted

--- src/ruby_supportlib/phusion_passenger/standalone/start_command/nginx_engine.rb
+++ src/ruby_supportlib/phusion_passenger/standalone/start_command/nginx_engine.rb
@@ -84,7 +84,7 @@ module PhusionPassenger
               end
             end
           end
-        rescue Errno::ECONNREFUSED, Errno::ECONNRESET
+        rescue Errno::ECONNREFUSED, Errno::ECONNRESET, Errno::ENOENT
         end
 
 
