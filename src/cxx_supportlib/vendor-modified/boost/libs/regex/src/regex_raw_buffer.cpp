78e70c4cdd17951eec5a3100ac5747fea7383059
Hongli Lai (Phusion) <hongli@phusion.nl>
2015-11-27 10:01:06 +0100

    Get rid of 'register' keyword which triggers compiler warnings

--- src/cxx_supportlib/vendor-modified/boost/libs/regex/src/regex_raw_buffer.cpp
+++ src/cxx_supportlib/vendor-modified/boost/libs/regex/src/regex_raw_buffer.cpp
@@ -3,8 +3,8 @@
  * Copyright (c) 2004
  * John Maddock
  *
- * Use, modification and distribution are subject to the 
- * Boost Software License, Version 1.0. (See accompanying file 
+ * Use, modification and distribution are subject to the
+ * Boost Software License, Version 1.0. (See accompanying file
  * LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
  *
  */
@@ -36,15 +36,15 @@ namespace boost{ namespace re_detail{
 
 void BOOST_REGEX_CALL raw_storage::resize(size_type n)
 {
-   register size_type newsize = start ? last - start : 1024;
+   size_type newsize = start ? last - start : 1024;
    while(newsize < n)
       newsize *= 2;
-   register size_type datasize = end - start;
+   size_type datasize = end - start;
    // extend newsize to WORD/DWORD boundary:
    newsize = (newsize + padding_mask) & ~(padding_mask);
 
    // allocate and copy data:
-   register pointer ptr = static_cast<pointer>(::operator new(newsize));
+   pointer ptr = static_cast<pointer>(::operator new(newsize));
    BOOST_REGEX_NOEH_ASSERT(ptr)
    if(start)
       std::memcpy(ptr, start, datasize);
@@ -63,7 +63,7 @@ void* BOOST_REGEX_CALL raw_storage::insert(size_type pos, size_type n)
    BOOST_ASSERT(pos <= size_type(end - start));
    if(size_type(last - end) < n)
       resize(n + (end - start));
-   register void* result = start + pos;
+   void* result = start + pos;
    std::memmove(start + pos + n, start + pos, (end - start) - pos);
    end += n;
    return result;
