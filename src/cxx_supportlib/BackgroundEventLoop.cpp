7dca92c10d6c32509b590090dc6c5403899d9136
Steven Chamberlain <steven@pyro.eu.org>
2016-02-21 20:19:28 +0000

    add support for GNU/kFreeBSD
    Use kqueue on any platform having the FreeBSD kernel;  not just
    FreeBSD itself.
    
    Also define __SYS_QUEUE_H_ as an additional guard macro in
    psg_sysqueue.h, which is something used by GNU/kFreeBSD only.

--- src/cxx_supportlib/BackgroundEventLoop.cpp
+++ src/cxx_supportlib/BackgroundEventLoop.cpp
@@ -43,7 +43,7 @@
 #ifndef HAVE_KQUEUE
 	#if defined(__APPLE__) || \
 		defined(__DragonFly__) || \
-		defined(__FreeBSD__) || \
+		defined(__FreeBSD_kernel__) || \
 		defined(__OpenBSD__) || \
 		defined(__NetBSD__)
 		#define HAVE_KQUEUE 1
