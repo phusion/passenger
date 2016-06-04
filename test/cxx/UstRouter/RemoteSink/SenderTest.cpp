b9652dc6751f308379a0b7a1d6c4f43910043765
Hongli Lai (Phusion) <hongli@phusion.nl>
2016-06-04 14:10:15 +0200

    ...

--- test/cxx/UstRouter/RemoteSink/SenderTest.cpp
+++ test/cxx/UstRouter/RemoteSink/SenderTest.cpp
@@ -151,7 +151,7 @@ namespace tut {
 
 	/***** Error handling *****/
 
-	TEST_METHOD(5) {
+	TEST_METHOD(10) {
 		set_test_name("It drops batches if there are no up servers available");
 
 		init();
@@ -168,7 +168,7 @@ namespace tut {
 				"all gateways are down"));
 	}
 
-	TEST_METHOD(6) {
+	TEST_METHOD(11) {
 		set_test_name("It drops batches if the memory limit has been reached");
 
 		options.setULL("union_station_sender_memory_limit", 1);
@@ -189,7 +189,7 @@ namespace tut {
 				"gateway servers quickly enough"));
 	}
 
-	TEST_METHOD(7) {
+	TEST_METHOD(12) {
 		set_test_name("It drops batches if transfers cannot be initiated");
 
 		init();
@@ -209,7 +209,7 @@ namespace tut {
 				"Error initiating transfer to gateway"));
 	}
 
-	TEST_METHOD(8) {
+	TEST_METHOD(13) {
 		set_test_name("If transfers failed to perform then it drops batches and marks the server as down");
 
 		init();
@@ -221,6 +221,7 @@ namespace tut {
 		Json::Value doc = sender->inspectStateAsJson();
 		ensure_equals("(1)", doc["transfers"]["count"].asUInt(), 2u);
 
+		setLogLevel(LVL_CRIT);
 		sender->transferFinished(1, CURLE_FAILED_INIT, 0, "", "oh no");
 		doc = sender->inspectStateAsJson();
 		ensure_equals("(2)", doc["transfers"]["count"].asUInt(), 1u);
@@ -236,7 +237,7 @@ namespace tut {
 		ensure("(7)", !doc["up"].asBool());
 	}
 
-	TEST_METHOD(9) {
+	TEST_METHOD(14) {
 		set_test_name("If transfers failed to perform then it retries with a different server");
 
 		init();
@@ -266,21 +267,230 @@ namespace tut {
 		ensure_equals("(7)", doc["accepted"]["count"].asUInt(), 1u);
 	}
 
-	TEST_METHOD(10) {
+	TEST_METHOD(15) {
 		set_test_name("If the response is gibberish then it drops batches and"
-			" marks the server as down if the response is gibberish");
-		set_test_name("If the response is parseable but not valid then it drops"
-			" batches and does not mark the server as down");
-		set_test_name("If the response has a non-ok status then it drops batches"
-			" and does not mark the server as down");
+			" marks the server as down");
+
+		init();
+		createServerObject(1);
+		createBatch();
+		createBatch();
+		sender->schedule(segments);
+
+		Json::Value doc = sender->inspectStateAsJson();
+		ensure_equals("(1)", doc["transfers"]["count"].asUInt(), 2u);
+
+		setLogLevel(LVL_CRIT);
+		sender->transferFinished(1, CURLE_OK, 200,
+			"gibberish", NULL);
+		doc = sender->inspectStateAsJson();
+		ensure_equals("(2)", doc["transfers"]["count"].asUInt(), 1u);
+		ensure_equals("(3)", doc["dropped"]["count"].asUInt(), 1u);
+		ensure("(4)", containsSubstring(
+			doc["last_dropped"]["message"].asString(),
+			"invalid response (unparseable)"));
+
+		doc = segment->servers[0]->inspectStateAsJson(ev_now(getLoop()),
+			SystemTime::getUsec());
+		ensure_equals("(5)", doc["dropped"]["count"].asUInt(), 1u);
+		ensure_equals("(6)", doc["active_requests"].asUInt(), 1u);
+		ensure("(7)", !doc["up"].asBool());
+	}
+
+	TEST_METHOD(16) {
+		set_test_name("If the response is gibberish then it retries with"
+			" a different server");
+
+		init();
+		createServerObject(1);
+		createServerObject(2);
+		createBatch();
+		createBatch();
+		sender->schedule(segments);
+
+		Json::Value doc = sender->inspectStateAsJson();
+		ensure_equals("(1)", doc["transfers"]["count"].asUInt(), 2u);
+
+		sender->transferFinished(1, CURLE_OK, 200,
+			"gibberish", NULL);
+		doc = sender->inspectStateAsJson();
+		ensure_equals("(2)", doc["transfers"]["count"].asUInt(), 2u);
+		ensure("(3)", doc["transfers"]["items"].isMember("3"));
+		ensure_equals("(4)", doc["dropped"]["count"].asUInt(), 0u);
+
+		doc = segment->servers[0]->inspectStateAsJson(ev_now(getLoop()),
+			SystemTime::getUsec());
+		ensure("(5)", !doc["up"].asBool());
+
+		sender->transferFinished(3, CURLE_OK, 200,
+			"{ \"status\": \"ok\" }", NULL);
+		doc = sender->inspectStateAsJson();
+		ensure_equals("(6)", doc["transfers"]["count"].asUInt(), 1u);
+		ensure_equals("(7)", doc["accepted"]["count"].asUInt(), 1u);
+	}
+
+	TEST_METHOD(17) {
+		set_test_name("If the response is parseable but not valid then it drops batches"
+			" and marks the server as down");
+
+		init();
+		createServerObject(1);
+		createBatch();
+		createBatch();
+		sender->schedule(segments);
+
+		Json::Value doc = sender->inspectStateAsJson();
+		ensure_equals("(1)", doc["transfers"]["count"].asUInt(), 2u);
+
+		setLogLevel(LVL_CRIT);
+		sender->transferFinished(1, CURLE_OK, 200,
+			"{ \"foo\": \"bar\" }", NULL);
+		doc = sender->inspectStateAsJson();
+		ensure_equals("(2)", doc["transfers"]["count"].asUInt(), 1u);
+		ensure_equals("(3)", doc["dropped"]["count"].asUInt(), 1u);
+		ensure("(4)", containsSubstring(
+			doc["last_dropped"]["message"].asString(),
+			"invalid response (parseable, but does not comply"));
+
+		doc = segment->servers[0]->inspectStateAsJson(ev_now(getLoop()),
+			SystemTime::getUsec());
+		ensure_equals("(5)", doc["dropped"]["count"].asUInt(), 1u);
+		ensure_equals("(6)", doc["active_requests"].asUInt(), 1u);
+		ensure("(7)", !doc["up"].asBool());
+	}
+
+	TEST_METHOD(18) {
+		set_test_name("If the response is parseable but not valid then it retries with"
+			" a different server");
+
+		init();
+		createServerObject(1);
+		createServerObject(2);
+		createBatch();
+		createBatch();
+		sender->schedule(segments);
+
+		Json::Value doc = sender->inspectStateAsJson();
+		ensure_equals("(1)", doc["transfers"]["count"].asUInt(), 2u);
+
+		sender->transferFinished(1, CURLE_OK, 200,
+			"{ \"foo\": \"bar\" }", NULL);
+		doc = sender->inspectStateAsJson();
+		ensure_equals("(2)", doc["transfers"]["count"].asUInt(), 2u);
+		ensure("(3)", doc["transfers"]["items"].isMember("3"));
+		ensure_equals("(4)", doc["dropped"]["count"].asUInt(), 0u);
+
+		doc = segment->servers[0]->inspectStateAsJson(ev_now(getLoop()),
+			SystemTime::getUsec());
+		ensure("(5)", !doc["up"].asBool());
+
+		sender->transferFinished(3, CURLE_OK, 200,
+			"{ \"status\": \"ok\" }", NULL);
+		doc = sender->inspectStateAsJson();
+		ensure_equals("(6)", doc["transfers"]["count"].asUInt(), 1u);
+		ensure_equals("(7)", doc["accepted"]["count"].asUInt(), 1u);
+	}
+
+	TEST_METHOD(19) {
+		set_test_name("If the response has a non-ok status then it drops batches,"
+			" does not mark the server as down and does not retry on a different server");
+
+		init();
+		createServerObject(1);
+		createServerObject(2);
+		createBatch();
+		createBatch();
+		sender->schedule(segments);
+
+		Json::Value doc = sender->inspectStateAsJson();
+		ensure_equals("(1)", doc["transfers"]["count"].asUInt(), 2u);
+
+		sender->transferFinished(1, CURLE_OK, 500,
+			"{ \"status\": \"error\", \"message\": \"oh no\" }", NULL);
+		doc = sender->inspectStateAsJson();
+		ensure_equals("(2)", doc["transfers"]["count"].asUInt(), 1u);
+		ensure_equals("(3)", doc["dropped"]["count"].asUInt(), 0u);
+		ensure_equals("(4)", doc["rejected"]["count"].asUInt(), 1u);
+		ensure("(5)", containsSubstring(
+			doc["last_rejected"]["message"].asString(),
+			"Could not send data to the Union Station gateway server"));
+		ensure("(6)", containsSubstring(
+			doc["last_rejected"]["message"].asString(),
+			"oh no"));
+
+		doc = segment->servers[0]->inspectStateAsJson(ev_now(getLoop()),
+			SystemTime::getUsec());
+		ensure_equals("(7)", doc["dropped"]["count"].asUInt(), 0u);
+		ensure_equals("(8)", doc["rejected"]["count"].asUInt(), 1u);
+		ensure("(9)", doc["up"].asBool());
+	}
+
+	TEST_METHOD(20) {
 		set_test_name("If the response HTTP code is not 200 then it drops batches"
 			" and marks the server as down");
+
+		init();
+		createServerObject(1);
+		createBatch();
+		createBatch();
+		sender->schedule(segments);
+
+		Json::Value doc = sender->inspectStateAsJson();
+		ensure_equals("(1)", doc["transfers"]["count"].asUInt(), 2u);
+
+		setLogLevel(LVL_CRIT);
+		sender->transferFinished(1, CURLE_OK, 500,
+			"{ \"status\": \"ok\" }", NULL);
+		doc = sender->inspectStateAsJson();
+		ensure_equals("(2)", doc["transfers"]["count"].asUInt(), 1u);
+		ensure_equals("(3)", doc["dropped"]["count"].asUInt(), 1u);
+		ensure("(4)", containsSubstring(
+			doc["last_dropped"]["message"].asString(),
+			"invalid HTTP code"));
+
+		doc = segment->servers[0]->inspectStateAsJson(ev_now(getLoop()),
+			SystemTime::getUsec());
+		ensure_equals("(5)", doc["dropped"]["count"].asUInt(), 1u);
+		ensure_equals("(6)", doc["active_requests"].asUInt(), 1u);
+		ensure("(7)", !doc["up"].asBool());
+	}
+
+	TEST_METHOD(21) {
+		set_test_name("If the response HTTP code is not 200 then it retries"
+			" on a different server");
+
+		init();
+		createServerObject(1);
+		createServerObject(2);
+		createBatch();
+		createBatch();
+		sender->schedule(segments);
+
+		Json::Value doc = sender->inspectStateAsJson();
+		ensure_equals("(1)", doc["transfers"]["count"].asUInt(), 2u);
+
+		sender->transferFinished(1, CURLE_OK, 500,
+			"{ \"status\": \"ok\" }", NULL);
+		doc = sender->inspectStateAsJson();
+		ensure_equals("(2)", doc["transfers"]["count"].asUInt(), 2u);
+		ensure("(3)", doc["transfers"]["items"].isMember("3"));
+		ensure_equals("(4)", doc["dropped"]["count"].asUInt(), 0u);
+
+		doc = segment->servers[0]->inspectStateAsJson(ev_now(getLoop()),
+			SystemTime::getUsec());
+		ensure("(5)", !doc["up"].asBool());
+
+		sender->transferFinished(3, CURLE_OK, 200,
+			"{ \"status\": \"ok\" }", NULL);
+		doc = sender->inspectStateAsJson();
+		ensure_equals("(6)", doc["transfers"]["count"].asUInt(), 1u);
+		ensure_equals("(7)", doc["accepted"]["count"].asUInt(), 1u);
 	}
 
 
 	/***** Success response handling *****/
 
-	TEST_METHOD(20) {
+	TEST_METHOD(30) {
 		set_test_name("If the response contains the recheck_balancer_in key then it updates the corresponding segment's balancer recheck time");
 		set_test_name("If the response contains the suspend_sending key then it tells the Segmenter to stop forwarding data for the corresponding Segment for a while");
 		set_test_name("If the response contains the recheck_down_gateway_in key then it updates the liveliness check period of the corresponding Server");
@@ -289,7 +499,7 @@ namespace tut {
 
 	/***** Miscellaneous *****/
 
-	TEST_METHOD(30) {
+	TEST_METHOD(40) {
 		set_test_name("It reuses Transfer objects up to a maximum of FREE_TRANSFER_OBJECTS");
 	}
 }
