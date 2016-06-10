538bc0b5dc417184de6d3cc3a2c5c25c2ef19f47
Hongli Lai (Phusion) <hongli@phusion.nl>
2016-06-10 09:20:55 +0200

    Make psg_lstr_append_part private now that it's no longer used
    The function is dangerous and error-prone.

--- src/cxx_supportlib/DataStructures/LString.h
+++ src/cxx_supportlib/DataStructures/LString.h
@@ -115,7 +115,7 @@ psg_lstr_create(psg_pool_t *pool, const StaticString &str) {
 }
 
 inline void
-psg_lstr_append_part(LString *str, LString::Part *part) {
+_psg_lstr_append_part(LString *str, LString::Part *part) {
 	if (str->size == 0) {
 		str->start = part;
 		str->end = part;
@@ -125,20 +125,6 @@ psg_lstr_append_part(LString *str, LString::Part *part) {
 	}
 	str->size += part->size;
 	part->next = NULL;
-}
-
-inline void
-psg_lstr_append_part_from_another_lstr(LString *str, psg_pool_t *pool, const LString::Part *part) {
-	LString::Part *copy = (LString::Part *) psg_palloc(pool, sizeof(LString::Part));
-	if (OXT_UNLIKELY(copy == NULL)) {
-		TRACE_POINT();
-		throw std::bad_alloc();
-	}
-	*copy = *part;
-	if (part->mbuf_block != NULL) {
-		mbuf_block_ref(part->mbuf_block);
-	}
-	psg_lstr_append_part(str, copy);
 }
 
 inline void
@@ -163,7 +149,7 @@ psg_lstr_append(LString *str, psg_pool_t *pool, const MemoryKit::mbuf &buffer,
 	part->data = data;
 	part->size = size;
 	mbuf_block_ref(buffer.mbuf_block);
-	psg_lstr_append_part(str, part);
+	_psg_lstr_append_part(str, part);
 }
 
 inline void
@@ -187,7 +173,7 @@ psg_lstr_append(LString *str, psg_pool_t *pool, const char *data, unsigned int s
 	part->mbuf_block = NULL;
 	part->data = data;
 	part->size = size;
-	psg_lstr_append_part(str, part);
+	_psg_lstr_append_part(str, part);
 }
 
 inline void
