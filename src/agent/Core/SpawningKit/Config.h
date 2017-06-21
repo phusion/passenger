03099ca50bf01d75942025c08c362da4ddd37dfa
Hongli Lai (Phusion) <hongli@phusion.nl>
2017-06-21 15:22:43 +0200

    Fix some copyright headers and comments

--- src/agent/Core/SpawningKit/Config.h
+++ src/agent/Core/SpawningKit/Config.h
@@ -1,3 +1,28 @@
+/*
+ *  Phusion Passenger - https://www.phusionpassenger.com/
+ *  Copyright (c) 2011-2017 Phusion Holding B.V.
+ *
+ *  "Passenger", "Phusion Passenger" and "Union Station" are registered
+ *  trademarks of Phusion Holding B.V.
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
 #ifndef _PASSENGER_SPAWNING_KIT_CONFIG_H_
 #define _PASSENGER_SPAWNING_KIT_CONFIG_H_
 
@@ -309,6 +334,7 @@ public:
 		  lveMinUid(DEFAULT_LVE_MIN_UID),
 		  fileDescriptorUlimit(0),
 		  startTimeoutMsec(DEFAULT_START_TIMEOUT)
+		  /*********************/
 		{ }
 
 	void internStrings();
