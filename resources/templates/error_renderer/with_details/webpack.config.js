8bef0dc441e1de8653dc5d003170f2563b5becab
Hongli Lai (Phusion) <hongli@phusion.nl>
2017-06-23 10:41:02 +0200

    webpack.config.js: fix indentation per EditorConfig specification
    [ci:skip]

--- resources/templates/error_renderer/with_details/webpack.config.js
+++ resources/templates/error_renderer/with_details/webpack.config.js
@@ -5,38 +5,38 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const extractCSS = new ExtractTextPlugin('styles.css');
 
 module.exports = {
-	entry: ['./src/index.js'],
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['env', 'react']
-					}
-				}
-			},
-			{
-				test: /\.css$/,
-				use: extractCSS.extract({
-					use: {
-						loader: 'css-loader',
-						options: {
-							minimize: true
-						}
-					},
-					fallback: 'style-loader'
-				})
-			},
-		]
-	},
-	plugins: [
-		extractCSS,
-		new UglifyJSPlugin()
-	]
+  entry: ['./src/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: extractCSS.extract({
+          use: {
+            loader: 'css-loader',
+            options: {
+              minimize: true
+            }
+          },
+          fallback: 'style-loader'
+        })
+      },
+    ]
+  },
+  plugins: [
+    extractCSS,
+    new UglifyJSPlugin()
+  ]
 };
