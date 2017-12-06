345ffedf6351ed1d2aedc41223765c4169da46e3
Daniel Knoppel (Phusion) <daniel@phusion.nl>
2017-12-06 18:14:18 +0100

    Tests: PoolTest 68F often fails needlessly; allow more evaluation time

--- test/cxx/Core/ApplicationPool/PoolTest.cpp
+++ test/cxx/Core/ApplicationPool/PoolTest.cpp
@@ -1609,13 +1609,13 @@ namespace tut {
 
 		LoggingKit::setLevel(LoggingKit::CRIT);
 		pool->asyncGet(options, callback);
-		EVENTUALLY(5,
+		EVENTUALLY(10,
 			result = number == 1;
 		);
-		EVENTUALLY(5,
+		EVENTUALLY(10,
 			result = pool->getProcessCount() == 2;
 		);
-		EVENTUALLY(5,
+		EVENTUALLY(10,
 			result = !pool->isSpawning();
 		);
 		SHOULD_NEVER_HAPPEN(500,
