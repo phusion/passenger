anbangisak/passenger-1

don’t escape wsgi uris in passenger

src/apache2_module/Hooks.cpp
@@ -64,6 +64,7 @@
 #include <Logging.h>
 #include <WatchdogLauncher.h>
 #include <Constants.h>
+#include <AppTypes.h>
 
 /* The Apache/APR headers *must* come after the Boost headers, otherwise
  * compilation will fail on OpenBSD.
@@ -852,7 +853,10 @@ private:
 		result.append(r->method);
 		result.append(" ", 1);
 
-		if (config->allowsEncodedSlashes()) {
+		if (mapper.getApplicationType() == PAT_WSGI) {
+			// wsgi apps don't want escaped URIs
+			result.append(r->uri);
+		} else if (config->allowsEncodedSlashes()) {
 			/*
 			 * Apache decodes encoded slashes in r->uri, so we must use r->unparsed_uri
 			 * if we are to support encoded slashes. However mod_rewrite doesn't change

phusion/passenger

src/apache2_module/Hooks.cpp
@@ -72,6 +72,7 @@
 #include <LoggingKit/Context.h>
 #include <WatchdogLauncher.h>
 #include <Constants.h>
+#include <AppTypes.h>
 
 /* The Apache/APR headers *must* come after the Boost headers, otherwise
  * compilation will fail on OpenBSD.
@@ -869,7 +870,14 @@ private:
 		result.append(r->method);
 		result.append(" ", 1);
 
-		if (config->getAllowEncodedSlashes()) {
+		if (mapper.getApplicationType() == PAT_WSGI) {
+			// wsgi apps don't want escaped URIs
+			result.append(r->uri);
+			if (r->args != NULL) {
+				result.append("?", 1);
+				result.append(r->args);
+			}
+		} else if (config->getAllowEncodedSlashes()) {
 			/*
 			 * Apache decodes encoded slashes in r->uri, so we must use r->unparsed_uri
 			 * if we are to support encoded slashes. However mod_rewrite doesn't change