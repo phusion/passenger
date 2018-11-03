69ac0da57033ed4b53db8e8a056d11fba26669ac
Po-Chuan Hsieh <sunpoet@sunpoet.net>
2018-11-03 18:24:15 +0800

    Fix build with curl 7.62.0
    from CHANGES:
    ssl: deprecate CURLE_SSL_CACERT in favour of a unified error code
    Long live CURLE_PEER_FAILED_VERIFICATION

--- src/agent/Core/SecurityUpdateChecker.h
+++ src/agent/Core/SecurityUpdateChecker.h
@@ -256,9 +256,11 @@ private:
 				error.append(" for proxy address " + sessionState.config["proxy_url"].asString());
 				break;
 
+#if LIBCURL_VERSION_NUM < 0x073e00
 			case CURLE_SSL_CACERT:
 				// Peer certificate cannot be authenticated with given / known CA certificates. This would happen
 				// for MITM but could also be a truststore issue.
+#endif
 			case CURLE_PEER_FAILED_VERIFICATION:
 				// The remote server's SSL certificate or SSH md5 fingerprint was deemed not OK.
 				error.append(" while connecting to " + sessionState.configRlz.url
