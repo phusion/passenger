2165cb3f6cce1ce5db2736fd4c1f49d5aaad6e50
Camden Narzt <c.narzt@me.com>
2018-12-18 09:19:41 -0700

    Add more container detection. Closes GH-2143.

--- src/cxx_supportlib/SystemTools/ContainerHelpers.h
+++ src/cxx_supportlib/SystemTools/ContainerHelpers.h
@@ -10,19 +10,73 @@
 #ifndef _PASSENGER_SYSTEM_TOOLS_CONTAINER_HELPERS_H_
 #define _PASSENGER_SYSTEM_TOOLS_CONTAINER_HELPERS_H_
 
+#include <unistd.h>
 #include <boost/predef.h>
 #include <FileTools/FileManip.h>
+#include <StrIntTools/StrIntUtils.h>
 
 namespace Passenger {
 
 using namespace std;
 
 
+// adapted from https://github.com/systemd/systemd/blob/042ca/src/basic/virt.c
+// separate function to keep as similar to source as possible for easier updates
+inline bool
+_linuxAutoDetectInContainer() {
+	// https://github.com/moby/moby/issues/26102#issuecomment-253621560
+	if (fileExists("/.dockerenv")) {
+		return true;
+	}
+
+	if (fileExists("/proc/vz") && !fileExists("/proc/bc")) {
+		return true;
+	}
+
+	const char *env = getenv("container");
+	if (env != NULL) {
+		return (*env != '\0');
+	}
+
+	if (fileExists("/run/systemd/container")) {
+		string file = unsafeReadFile("/run/systemd/container");
+		return (file.length() > 0);
+	}
+
+	if (fileExists("/proc/1/environ")) {
+		string file = unsafeReadFile("/proc/1/environ");
+		if (file.size() > 0) {
+			vector<string> v;
+			split(file,'\0', v);
+			for(vector<string>::iterator it = v.begin(); it != v.end(); ++it) {
+				if (startsWith(*it, "container=")) {
+					return true;
+				}
+			}
+		}
+	}
+
+	if (fileExists("/proc/1/sched")) {
+		string file = unsafeReadFile("/proc/1/sched");
+		if (file.length() >= 0) {
+			const char t = file[0];
+			if (t == '\0') {
+				return false;
+			}
+
+			if (!startsWith(file, "(1,")) {
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
 inline bool
 autoDetectInContainer() {
 	#if BOOST_OS_LINUX
-		// https://github.com/moby/moby/issues/26102#issuecomment-253621560
-		return fileExists("/.dockerenv");
+		return _linuxAutoDetectInContainer();
 	#else
 		return false;
 	#endif
