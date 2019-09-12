be47a77582bd5bcfff5fd942c1173d9847aa7f90
Camden Narzt <c.narzt@me.com>
2019-09-12 13:05:49 -0600

    Post release tasks

--- src/ruby_supportlib/phusion_passenger.rb
+++ src/ruby_supportlib/phusion_passenger.rb
@@ -31,7 +31,7 @@ module PhusionPassenger
 
   PACKAGE_NAME = 'passenger'
   # Run 'rake src/cxx_supportlib/Constants.h configkit_schemas_inline_comments' after changing this number.
-  VERSION_STRING = '6.0.3'
+  VERSION_STRING = '6.0.4'
 
   # Tip: find the SHA-256 with ./dev/nginx_version_sha2 <VERSION>
   PREFERRED_NGINX_VERSION = '1.17.3'
