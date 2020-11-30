ad4fabf328497b5bc95e377a1f4450e7d107f868
oxpa <iippolitov@gmail.com>
2020-11-30 00:14:08 +0000

    avoid using other modules directories

--- src/nginx_module/Configuration.c
+++ src/nginx_module/Configuration.c
@@ -61,8 +61,8 @@ static ngx_str_t headers_to_hide[] = {
 
 passenger_main_conf_t passenger_main_conf;
 
-static ngx_path_init_t  ngx_http_proxy_temp_path = {
-    ngx_string(NGX_HTTP_PROXY_TEMP_PATH), { 1, 2, 0 }
+static ngx_path_init_t  ngx_http_passenger_temp_path = {
+    ngx_string(NGX_HTTP_PASSENGER_TEMP_PATH), { 1, 2, 0 }
 };
 
 
@@ -580,7 +580,7 @@ passenger_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child)
     if (ngx_conf_merge_path_value(cf,
                               &conf->upstream_config.temp_path,
                               prev->upstream_config.temp_path,
-                              &ngx_http_proxy_temp_path)
+                              &ngx_http_passenger_temp_path)
         != NGX_OK)
     {
         return NGX_CONF_ERROR;
