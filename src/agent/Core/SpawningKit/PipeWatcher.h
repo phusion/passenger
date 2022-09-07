712408eed8ac2f358fc7d3bf7c8787f5bb2302ee
Fernando Nunes <fernando.nunes@zappistore.com>
2022-09-07 16:11:17 +0100

    Make log line limit 32K

--- src/agent/Core/SpawningKit/PipeWatcher.h
+++ src/agent/Core/SpawningKit/PipeWatcher.h
@@ -90,7 +90,7 @@ private:
 
 		UPDATE_TRACE_POINT();
 		while (!boost::this_thread::interruption_requested()) {
-			char buf[1024 * 60];
+			char buf[1024 * 32];
 			ssize_t ret;
 
 			UPDATE_TRACE_POINT();
