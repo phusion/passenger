7f50eb87f74f0aa4357921da1d942c98d95cddb1
Camden Narzt <c.narzt@me.com>
2024-01-03 15:35:57 -0700

    go back to concatenation

--- test/stub/wsgi/passenger_wsgi.py
+++ test/stub/wsgi/passenger_wsgi.py
@@ -99,10 +99,10 @@ def application(env, start_response):
 		body = b"This is the uncached version of /cached"
 	elif path == '/upload_with_params':
 		params = cgi.FieldStorage(fp = env['wsgi.input'], environ = env)
-		name1 = str(params["name1"].value)
-		name2 = str(params["name2"].value)
-		data  = str(params["data"].value)
-		body = str_to_bytes("name 1 = %s\nname 2 = %s\ndata = %s" % (name1, name2, data))
+		name1 = str_to_bytes(params["name1"].value)
+		name2 = str_to_bytes(params["name2"].value)
+		data  = str_to_bytes(params["data"].value)
+		body  = b"name 1 = " + name1 + b"\nname 2 = " + name2 + b"\ndata = " + data
 	elif path == '/raw_upload_to_file':
 		sleep_time = float(env.get('HTTP_X_SLEEP', 0))
 		f = open(env['HTTP_X_OUTPUT'], 'wb')
