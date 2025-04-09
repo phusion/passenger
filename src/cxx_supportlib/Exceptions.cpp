726ca1e37fb45b888b847c7819f60542dd16ab27
Hongli Lai <hongli@phusion.nl>
2025-04-09 05:44:54 +0200

    Refactor Exceptions.h
    - Use noexcept
    - Move implementation to .cpp file
    - Shorten descriptions

--- src/cxx_supportlib/Exceptions.cpp
+++ src/cxx_supportlib/Exceptions.cpp
@@ -27,6 +27,9 @@
 #include <Exceptions.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sstream>
+#include <cstring>
+#include <cassert>
 
 
 void
@@ -70,3 +73,178 @@ pp_error_set(const std::exception &ex, PP_Error *error) {
 		error->errnoCode = PP_NO_ERRNO;
 	}
 }
+
+
+namespace Passenger {
+
+SystemException::SystemException(const string &briefMessage, int errorCode) {
+	stringstream str;
+	str << strerror(errorCode) << " (errno=" << errorCode << ")";
+	systemMessage = str.str();
+	setBriefMessage(briefMessage);
+	m_code = errorCode;
+}
+
+SystemException::~SystemException() noexcept {}
+
+const char *
+SystemException::what() const noexcept {
+	return fullMessage.c_str();
+}
+
+void
+SystemException::setBriefMessage(const string &message) {
+	briefMessage = message;
+	fullMessage = briefMessage + ": " + systemMessage;
+}
+
+int
+SystemException::code() const noexcept {
+	return m_code;
+}
+
+string
+SystemException::brief() const noexcept {
+	return briefMessage;
+}
+
+string
+SystemException::sys() const noexcept {
+	return systemMessage;
+}
+
+
+FileSystemException::FileSystemException(const string &message, int errorCode, const string &filename)
+	: SystemException(message, errorCode), m_filename(filename) {}
+
+FileSystemException::~FileSystemException() noexcept {}
+
+string
+FileSystemException::filename() const noexcept {
+	return m_filename;
+}
+
+
+TimeRetrievalException::TimeRetrievalException(const string &message, int errorCode)
+	: SystemException(message, errorCode) {}
+
+TimeRetrievalException::~TimeRetrievalException() noexcept {}
+
+
+IOException::IOException(const string &message) : msg(message) {}
+
+IOException::~IOException() noexcept {}
+
+const char *
+IOException::what() const noexcept {
+	return msg.c_str();
+}
+
+
+FileNotFoundException::FileNotFoundException(const string &message) : IOException(message) {}
+
+FileNotFoundException::~FileNotFoundException() noexcept {}
+
+
+EOFException::EOFException(const string &message) : IOException(message) {}
+
+EOFException::~EOFException() noexcept {}
+
+
+ConfigurationException::ConfigurationException(const string &message) : msg(message) {}
+
+ConfigurationException::~ConfigurationException() noexcept {}
+
+const char *
+ConfigurationException::what() const noexcept {
+	return msg.c_str();
+}
+
+
+GetAbortedException::GetAbortedException(const string &message) : msg(message) {}
+
+GetAbortedException::GetAbortedException(const oxt::tracable_exception::no_backtrace &tag)
+	: oxt::tracable_exception(tag) {}
+
+GetAbortedException::~GetAbortedException() noexcept {}
+
+const char *
+GetAbortedException::what() const noexcept {
+	return msg.c_str();
+}
+
+
+RequestQueueFullException::RequestQueueFullException(unsigned int maxQueueSize)
+	: GetAbortedException(oxt::tracable_exception::no_backtrace())
+{
+	stringstream str;
+	str << "Request queue full (configured max. size: " << maxQueueSize << ")";
+	msg = str.str();
+}
+
+RequestQueueFullException::~RequestQueueFullException() noexcept {}
+
+const char *
+RequestQueueFullException::what() const noexcept {
+	return msg.c_str();
+}
+
+
+ArgumentException::ArgumentException(const string &message) : msg(message) {}
+
+ArgumentException::~ArgumentException() noexcept {}
+
+const char *
+ArgumentException::what() const noexcept {
+	return msg.c_str();
+}
+
+
+InvalidModeStringException::InvalidModeStringException(const string &message) : ArgumentException(message) {}
+
+
+RuntimeException::RuntimeException(const string &message) : msg(message) {}
+
+RuntimeException::~RuntimeException() noexcept {}
+
+const char *
+RuntimeException::what() const noexcept {
+	return msg.c_str();
+}
+
+
+TimeoutException::TimeoutException(const string &message) : msg(message) {}
+
+TimeoutException::~TimeoutException() noexcept {}
+
+const char *
+TimeoutException::what() const noexcept {
+	return msg.c_str();
+}
+
+
+SecurityException::SecurityException(const string &message) : msg(message) {}
+
+SecurityException::~SecurityException() noexcept {}
+
+const char *
+SecurityException::what() const noexcept {
+	return msg.c_str();
+}
+
+
+NonExistentUserException::NonExistentUserException(const string &message) : SecurityException(message) {}
+
+NonExistentGroupException::NonExistentGroupException(const string &message) : SecurityException(message) {}
+
+
+SyntaxError::SyntaxError(const string &message) : msg(message) {}
+
+SyntaxError::~SyntaxError() noexcept {}
+
+const char *
+SyntaxError::what() const noexcept {
+	return msg.c_str();
+}
+
+} // namespace Passenger
