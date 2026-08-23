da94c9aa26bea04bd5bea0e3d8a27c1d3b0740ed
Camden Narzt <c.narzt@me.com>
2026-08-23 16:10:37 -0600

    Allow libev to compile on FreeBSD

--- src/cxx_supportlib/vendor-modified/libev/ev.c
+++ src/cxx_supportlib/vendor-modified/libev/ev.c
@@ -420,7 +420,11 @@
 #endif
 
 #if EV_USE_INOTIFY
-# include <sys/statfs.h>
+# ifdef __FreeBSD__
+#  include <sys/mount.h>
+# else
+#  include <sys/statfs.h>
+# endif
 # include <sys/inotify.h>
 /* some very old inotify.h headers don't have IN_DONT_FOLLOW */
 # ifndef IN_DONT_FOLLOW
